c71cea543c4c4110f91f46173ae30d0e101fb2f4
Gerald Versluis <gerald@verslu.is>
2016-11-17 09:47:40 +0100

    - Added Gerald Versluis as author - Made some specific Readify stuff configurable through the Web.config - Added Planet Xamarin logo

---
+++ src/Firehose.Web/Authors/GeraldVersluis.cs
@@ -0,0 +1,28 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+    public class GeraldVersluis : IAmAReadifarian
+    {
+        public string FirstName => "Gerald";
+
+        public string LastName => "Versluis";
+
+        public string StateOrRegion => "Holland";
+
+        public string EmailAddress => "gerald@verslu.is";
+
+        public string Title => "Xamarin Developer | Microsoft MVP";
+
+        public Uri WebSite => new Uri("https://blog.verslu.is/");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://blog.verslu.is/feed/"); }
+        }
+
+        public string TwitterHandle => "jfversluis";
+    }
+}
--- src/Firehose.Web/Infrastructure/GravatarHashingExtensions.cs
+++ src/Firehose.Web/Infrastructure/GravatarHashingExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 
 namespace Firehose.Web.Infrastructure
 {
@@ -8,7 +9,7 @@ namespace Firehose.Web.Infrastructure
         {
             var hashedEmailAddress = emailAddress.Trim().ToLowerInvariant().ToMd5Hash().ToLowerInvariant();
             const int size = 200;
-            var defaultImage = HttpUtility.UrlEncode("https://firehose.labs.readify.net/Content/Readify.png");
+            var defaultImage = HttpUtility.UrlEncode(ConfigurationManager.AppSettings["DefaultGravatarImage"]);
             return string.Format("http://www.gravatar.com/avatar/{0}.jpg?s={1}&d={2}", hashedEmailAddress, size, defaultImage);
         }
     }
