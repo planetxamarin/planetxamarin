ce32574dfc73fbf5f41bf443f63dcafc57360c0a
Tomasz Cielecki <tomasz@ostebaronen.dk>
2017-01-14 21:31:51 +0100

    Added Tomasz Cielecki

---
+++ src/Firehose.Web/Authors/TomaszCielecki.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class TomaszCielecki : IAmAXamarinMVP
+    {
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("http://blog.ostebaronen.dk/feeds/posts/default"); }
+        }
+
+        public string FirstName => "Tomasz";
+        public string LastName => "Cielecki";
+        public string StateOrRegion => "Copenhagen, Denmark";
+        public string EmailAddress => "tomasz@ostebaronen.dk";
+        public string Title => "Software Engineer";
+        public Uri WebSite => new Uri("http://blog.ostebaronen.dk");
+        public string TwitterHandle => "Cheesebaron";
+        public DateTime FirstAwarded => new DateTime(2015, 1, 1);
+    }
+}
