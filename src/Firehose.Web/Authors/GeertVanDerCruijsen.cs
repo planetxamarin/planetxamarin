af8eeac2244fc020c9008cb8b75a862bb061f44b
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-01-16 22:40:50 +0100

    Added Geert van der Cruijsen as Author

---
+++ src/Firehose.Web/Authors/GeertVanDerCruijsen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web
+{
+	public class GeertVanDerCruijsen : IAmACommunityMember, IFilterMyBlogPosts
+	{
+		public string FirstName => "Geert";
+		public string LastName => "van der Cruijsen";
+		public string Title => "Lead consultant Mobile & CLoud @ Xpirit";
+		public string EmailAddress => "";
+		public string TwitterHandle => "geertvdc";
+		public string GravatarHash => "ed3bedceb37c756ee04c8fad2a6b557f";
+		public string StateOrRegion => "Uden, Netherlands";
+		public Uri WebSite => new Uri("https://mobilefirstcloudfirst.net");
+
+		public IEnumerable<Uri> FeedUris
+		{
+			get { yield return new Uri("http://mobilefirstcloudfirst.net/feed//"); }
+		}
+
+		public bool Filter(SyndicationItem item) =>
+			item.Title.Text.ToLowerInvariant().Contains("xamarin") ||
+			item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
+	}
+}
