9e2ee8cd3861db7bb88bd0d72b0d9d88192b16c2
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-01-16 08:30:09 +0100

    Added new author

---
+++ src/Firehose.Web/Authors/JavierSuarez.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class TomaszCielecki : IAmAMicrosoftMVP
+    {
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://javiersuarezruiz.wordpress.com/feed/"); }
+        }
+
+        public string FirstName => "Javier";
+        public string LastName => "Suarez";
+        public string StateOrRegion => "Seville, Spain";
+        public string EmailAddress => "javiersuarezruiz@hotmail.com";
+        public string Title => "Software Developer";
+        public Uri WebSite => new Uri("http://javiersuarezruiz.wordpress.com");
+        public string TwitterHandle => "jsuarezruiz";
+        public DateTime FirstAwarded => new DateTime(2014, 07, 01);
+    }
+}
