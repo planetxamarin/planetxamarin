d49f1de6afcacc7eceb01fbedb8d6c001adc4812
Dan Rigby <dan@danrigby.com>
2017-01-17 13:54:54 -0500

    Add Dan Rigby

---
+++ src/Firehose.Web/Authors/DanRigby.cs
@@ -0,0 +1,25 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+    public class DanRigby : IWorkAtXamarinOrMicrosoft
+    {
+        public string FirstName => "Dan";
+        public string LastName => "Rigby";
+        public string Title => "Xamarin Technical Solutions Professional";
+        public DateTime Started => new DateTime(2015, 12, 1);
+        public string StateOrRegion => "North Carolina";
+        public string EmailAddress => "dan.rigby@xamarin.com";
+        public string TwitterHandle => "DanRigby";
+        public Uri WebSite => new Uri("http://danrigby.com/");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://feeds.feedburner.com/DanRigby"); }
+        }
+
+        public string GravatarHash => "f025f772418fbcfd3a1e15a74bf0f8a4";
+    }
+}
