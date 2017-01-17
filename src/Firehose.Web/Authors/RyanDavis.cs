ce76c434df4753aa0ced6b4e28af11038fbe4593
Ryan Davis <ryandavis.au@gmail.com>
2017-01-18 09:33:08 +1000

    filter rdavisau to xamarin only 🐒 (#89)

--- src/Firehose.Web/Authors/RyanDavis.cs
+++ src/Firehose.Web/Authors/RyanDavis.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
 using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class RyanDavis : IAmAXamarinMVP
+    public class RyanDavis : IAmAXamarinMVP, IFilterMyBlogPosts
     {
         public IEnumerable<Uri> FeedUris
         {
@@ -20,5 +22,7 @@ namespace Firehose.Web.Authors
         public string TwitterHandle => "rdavis_au";
         public DateTime FirstAwarded => new DateTime(2015, 1, 1);
         public string GravatarHash => "d351762ec451e252b20ff860dfcded91d351762ec451e252b20ff860dfcded91";
+        
+        public bool Filter(SyndicationItem item) => item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
     }
 }
