f6ebcc056c014f2d274c34ce2b02188ae8ba5952
S Ravi Kumar <srkrathore@gmail.com>
2017-01-17 17:05:57 +0530

    Removed multiple filters from Filter Method
    Mistakenly I committed the whole Filter method, updated it according to my need.

--- src/Firehose.Web/Authors/SRaviKumar.cs
+++ src/Firehose.Web/Authors/SRaviKumar.cs
@@ -13,14 +13,7 @@ public class BruceWayne : IAmACommunityMember, IFilterMyBlogPosts
     
     public bool Filter(SyndicationItem item)
     {
-        // Here you filter out the given item by the criteria you want, i.e.
-        // this filters out posts that do not have Xamarin in the title
-        return item.Title.Text.ToLowerInvariant().Contains("xamarin");
-
         // This filters out only the posts that have the "xamarin" category
         return item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
-
-        // Of course you can make the checks as complicated as you want and combine some stuff
-        return item.Title.Text.ToLowerInvariant().Contains("xamarin") && item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
     }
 }
