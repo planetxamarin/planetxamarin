2305d0aa8d2b2b261d293ec090e714b77213e010
Marc Bruins <marc@marcbruins.nl>
2017-01-17 10:39:15 +0100

    Add Marc Bruins

---
+++ src/Firehose.Web/Authors/MarcBruins.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web
+{
+	public class MarcBruins : IAmACommunityMember, IFilterMyBlogPosts
+	{
+		public string FirstName => "Marc";
+		public string LastName => "Bruins";
+		public string Title => "Xamarin Consultant";
+		public string EmailAddress => "marc@marcbruins.nl";
+		public string TwitterHandle => "MarcBruins";
+		public string GravatarHash => "3795d2031be87499f76f6336ec5a3a45";
+
+		public string StateOrRegion => "Groningen, Netherlands";
+		public Uri WebSite => new Uri("http://marcbruins.nl");
+
+		public IEnumerable<Uri> FeedUris
+		{
+			get { yield return new Uri("http://www.marcbruins.nl/feed/"); }
+		}
+
+		public bool Filter(SyndicationItem item) =>
+			item.Title.Text.ToLowerInvariant().Contains("xamarin") ||
+			item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
+	}
+}
