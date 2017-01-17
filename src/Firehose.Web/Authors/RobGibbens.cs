969e4ed574f1ca594d8175cad3e05e5f18be817b
Rob Gibbens <RobGibbens@gmail.com>
2017-01-16 22:20:53 -0500

    Create RobGibbens.cs

---
+++ src/Firehose.Web/Authors/RobGibbens.cs
@@ -0,0 +1,13 @@
+public class RobGibbens : IAmACommunityMember
+{
+    public string FirstName => "Rob";
+    public string LastName => "Gibbens";
+    public string Title => "Manager / Trainer @ Xamarin University";
+    public string StateOrRegion => "Farmington, Michigan";
+    public string EmailAddress => "RobGibbens@arteksoftware.com";
+    public string TwitterHandle => "RobGibbens";
+    public string GravatarHash => "";
+
+    public Uri WebSite => new Uri("http://arteksoftware.com/");
+    public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://arteksoftware.com/rss/"); } }
+}
