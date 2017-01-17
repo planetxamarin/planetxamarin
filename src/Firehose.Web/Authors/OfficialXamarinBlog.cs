a50d36f19cd341c485f583908259fe65cd176908
James Montemagno <james.montemagno@gmail.com>
2017-01-16 18:50:33 -0800

    Add Xamarin Blog, Merge Conflict, Gone Mobile, The Xamarin Show
    Transfer over from past PX

---
+++ src/Firehose.Web/Authors/OfficialXamarinBlog.cs
@@ -0,0 +1,31 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+	public class OfficialXamarinBlog : IAmACommunityMember
+    {
+        public string FirstName => "Xamarin";
+
+        public string LastName => "Blog";
+
+        public string StateOrRegion => "Internet";
+
+        public string EmailAddress => "hello@xamarin.com";
+
+        public string Title => "Official Blog";
+
+        public Uri WebSite => new Uri("http://blog.xamarin.com");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("http://blog.xamarin.com/feed/"); }
+        }
+
+        public string TwitterHandle => "xamarinhq";
+
+
+        public string GravatarHash => "70148d964bb389d42547834e1062c886";
+    }
+}
