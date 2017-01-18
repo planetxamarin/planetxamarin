7d9680c12227cfa0345f551ce56165bb6dd548fa
Gerald Versluis <gerald@verslu.is>
2017-01-18 20:56:35 +0100

    Bring some blogger info on Preview screen Did an attempt on showing some blogger info on the Preview page, surely could be optimized but it's a start
    ![Screenshot](http://i.imgur.com/061n5ZB.png)

---
+++ src/Firehose.Web/ViewModels/PreviewViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.ViewModels
+{
+    public class PreviewViewModel
+    {
+        public SyndicationFeed Feed { get; set; }
+        public IAmACommunityMember[] Bloggers { get; set; }
+    }
+}
