25b6c31f603a19ada828e1a08b3fe4bbd44228f9
michaelridland <rid00z@gmail.com>
2017-01-18 04:42:40 +0000

    Add Michael Ridland

---
+++ src/Firehose.Web/Authors/MichaelRidland.cs
@@ -0,0 +1,32 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+    public class MichaelRidland : IAmAXamarinMVP
+    {
+        public string FirstName => "Michael";
+
+        public string LastName => "Ridland";
+
+        public string StateOrRegion => "Sydney, Australia";
+
+        public string EmailAddress => "michael@xam-consulting.com";
+
+        public string Title => "Xamarin Developer / Xamarin MVP";
+
+        public Uri WebSite => new Uri("http://www.michaelridland.com");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("http://www.michaelridland.com/feed/"); }
+        }
+
+        public string TwitterHandle => "rid00z";
+
+        public DateTime FirstAwarded => new DateTime(2015, 4, 1);
+
+        public string GravatarHash => "";
+    }
+}
