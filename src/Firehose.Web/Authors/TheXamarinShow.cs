f92469309de4ecc7566ead387639f62ef3ff5ba0
James Montemagno <james.montemagno@gmail.com>
2017-01-18 10:57:38 -0800

    Update feed and gravatar (#108)

--- src/Firehose.Web/Authors/TheXamarinShow.cs
+++ src/Firehose.Web/Authors/TheXamarinShow.cs
@@ -12,19 +12,19 @@ namespace Firehose.Web.Authors
 
         public string StateOrRegion => "Channel 9";
 
-        public string EmailAddress => "planet@xamarin.com";
+        public string EmailAddress => "";
 
-        public string ShortBioOrTagLine => "Weekly Developer Show";
+        public string ShortBioOrTagLine => "Weekly Developer Show for Xamarin Developers";
 
         public Uri WebSite => new Uri("http://xamarinshow.com");
 
         public IEnumerable<Uri> FeedUris
         {
-            get { yield return new Uri("https://channel9.msdn.com/Shows/XamarinShow/RSS"); }
+            get { yield return new Uri("https://s.ch9.ms/Shows/XamarinShow/feed"); }
         }
 
         public string TwitterHandle => "thexamarinshow";
 
-        public string GravatarHash => "5db2c9a87693f1fd71f18df1e049b975";
+        public string GravatarHash => "7a0c7da0279b4e90439e780fa01924e0";
     }
 }
