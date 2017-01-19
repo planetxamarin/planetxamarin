4eeeead6a69d8ca454349c254e8091e1df9637cf
Peter Foot <peter@peterfoot.net>
2017-01-19 21:06:18 +0000

    Added Peter Foot as an author

---
+++ src/Firehose.Web/Authors/PeterFoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+using System.Linq;
+
+namespace Firehose.Web.Authors
+{
+	public class PeterFoot : IAmAMicrosoftMVP, IFilterMyBlogPosts
+	{
+		public string FirstName => "Peter";
+		public string LastName => "Foot";
+		public string Title => "Xamarin and Windows developer at In The Hand Ltd";
+		public string StateOrRegion => "United Kingdom";
+		public string EmailAddress => "peter@peterfoot.net";
+		public string TwitterHandle => "peterfoot";
+
+		public Uri WebSite => new Uri("http://peterfoot.net");
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://peterfoot.net/feed/"); } }
+
+		DateTime IAmAMicrosoftMVP.FirstAwarded => new DateTime(2003, 4, 1);
+
+		public string GravatarHash => "fa15aeeccc4b23e8a4677aeacb65b7bb";
+
+		public bool Filter(SyndicationItem item)
+		{
+			
+			return item.Categories.Where(i => i.Name.Equals("Xamarin", StringComparison.OrdinalIgnoreCase)).Any();
+		}
+	}
+}
