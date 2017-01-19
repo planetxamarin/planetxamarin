766fcc9a47e966c97737a74df660f8e724dd1f28
Beray Bentesen <beraybentesen@windowslive.com>
2017-01-20 02:20:16 +0300

    * BerayBentesen.cs: Author created
    * Web.config:
    * Firehose.Web.csproj:

---
+++ src/Firehose.Web/Authors/BerayBentesen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web
+{
+	public class BerayBentesen : IAmACommunityMember
+	{
+		public string FirstName => "Beray";
+		public string LastName => "Bentesen";
+		public string ShortBioOrTagLine => string.Empty;
+		public string StateOrRegion => "Istanbul, Turkey";
+		public string EmailAddress => "beraybentesen@windowslive.com";
+		public string TwitterHandle => "BerayBentesen";
+		public string GravatarHash => string.Empty;
+		public string GitHubHandle => "beraybentesen";
+		public GeoPosition Position => new GeoPosition(41.015137, 28.979530);
+
+		public Uri WebSite => new Uri("http://www.code2xamarin.net/");
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://code2xamarin.net/rss/"); } }
+	}
+}
