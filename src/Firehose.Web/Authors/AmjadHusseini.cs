83f69e4e1370d44cee8888532b3b091c0a407a00
Amjad <amjad.alhusseini@aramex.com>
2017-01-20 21:17:24 +0200

    Add Amjad Alhusseini as an author

---
+++ src/Firehose.Web/Authors/AmjadHusseini.cs
@@ -0,0 +1,13 @@
+public class AmjadHusseini : IAmACommunityMember
+{
+    public string FirstName => "Amjad";
+    public string LastName => "Al-Husseini";
+    public string ShortBioOrTagLine => "A Web/Mobile lead developer @aramex | Technology enthusiastic | Body builder and Guitar player";
+    public string StateOrRegion => "Jordan";
+    public string EmailAddress => "amjad.alhusseini@outlook.com";
+    public string TwitterHandle => "amjad_husseini";
+    public string GravatarHash => "7eb71ff7195835154692f3fe61a0dfa8";
+
+    public Uri WebSite => new Uri("https://amjadhusseini.com/");
+    public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://amjadhusseini.com/feed"); } }
+}
