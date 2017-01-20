d78d988767b09a0541b1b6b91e1ad0c80a66b271
li xiaobin <NewBLifes@hotmail.com>
2017-01-20 14:54:54 +0900

    add xamarin filter

--- src/Firehose.Web/Authors/LiXiaobin.cs
+++ src/Firehose.Web/Authors/LiXiaobin.cs
@@ -1,10 +1,12 @@
 ﻿using Firehose.Web.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
 
 namespace Firehose.Web.Authors
 {
-    public class LiXiaobin : IAmACommunityMember
+    public class LiXiaobin : IAmACommunityMember, IFilterMyBlogPosts
     {
         public string FirstName => "Li";
         public string LastName => "Xiaobin";
@@ -18,5 +20,9 @@ namespace Firehose.Web.Authors
         public Uri WebSite => new Uri("http://www.cnblogs.com/lixiaobin/");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://feed.cnblogs.com/blog/u/93829/rss"); } }
 
+        public bool Filter(SyndicationItem item)
+        {
+            return item.Categories.Any(c => c.Name.ToLowerInvariant().Contains("xamarin"));
+        }
     }
 }
