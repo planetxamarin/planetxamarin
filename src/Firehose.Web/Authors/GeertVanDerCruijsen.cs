8dfea285e5af8d3ab6c2616c711103c80a30eeba
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-01-20 12:49:00 +0100

    updated bio tag line

--- src/Firehose.Web/Authors/GeertVanDerCruijsen.cs
+++ src/Firehose.Web/Authors/GeertVanDerCruijsen.cs
@@ -6,7 +6,7 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web
 {
-<<<<<<< HEAD
+
 	public class GeertVanDerCruijsen : IAmACommunityMember, IFilterMyBlogPosts
 	{
 		public string FirstName => "Geert";
@@ -18,29 +18,19 @@ namespace Firehose.Web
 
 		public string StateOrRegion => "Uden, Netherlands";
 		public Uri WebSite => new Uri("https://mobilefirstcloudfirst.net");
-=======
-    public class GeertVanDerCruijsen : IAmACommunityMember, IFilterMyBlogPosts
-    {
-        public string FirstName => "Geert";
-        public string LastName => "van der Cruijsen";
-        public string ShortBioOrTagLine => string.Empty;
-        public string EmailAddress => "";
-        public string TwitterHandle => "geertvdc";
-        public string GravatarHash => "ec02820495ff6d50e58dd027aa2b0ae3";
->>>>>>> df2c7b1d1eafcbbdba8c074524d467e0e9285875
-
-        public string StateOrRegion => "Uden, Netherlands";
-        public Uri WebSite => new Uri("https://mobilefirstcloudfirst.net");
+		public string GitHubHandle => "geertvdc";
+		public string ShortBioOrTagLine => "Geert is a software architect who loves mobile & cloud. Also Xamarin University Training Partner";
 
         public IEnumerable<Uri> FeedUris
         {
             get { yield return new Uri("http://mobilefirstcloudfirst.net/feed"); }
         }
 
-        public string GitHubHandle => string.Empty;
+        
         public GeoPosition Position => new GeoPosition(51.6631070, 5.6239230);
 
-        public bool Filter(SyndicationItem item) =>
+
+		public bool Filter(SyndicationItem item) =>
             item.Title.Text.ToLowerInvariant().Contains("xamarin") ||
             item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
     }
