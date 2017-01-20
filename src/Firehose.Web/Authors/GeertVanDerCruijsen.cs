2c849cf6b89e0199ca1d6a49a7b82480238c58e7
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-01-20 23:01:05 +1100

    Update GeertVanDerCruijsen.cs

--- src/Firehose.Web/Authors/GeertVanDerCruijsen.cs
+++ src/Firehose.Web/Authors/GeertVanDerCruijsen.cs
@@ -11,7 +11,6 @@ namespace Firehose.Web
 	{
 		public string FirstName => "Geert";
 		public string LastName => "van der Cruijsen";
-		public string Title => "Lead consultant Mobile & Cloud @ Xpirit";
 		public string EmailAddress => "";
 		public string TwitterHandle => "geertvdc";
 		public string GravatarHash => "ec02820495ff6d50e58dd027aa2b0ae3";
@@ -19,7 +18,7 @@ namespace Firehose.Web
 		public string StateOrRegion => "Uden, Netherlands";
 		public Uri WebSite => new Uri("https://mobilefirstcloudfirst.net");
 		public string GitHubHandle => "geertvdc";
-		public string ShortBioOrTagLine => "Geert is a software architect who loves mobile & cloud. Also Xamarin University Training Partner";
+		public string ShortBioOrTagLine => "is Xamarin University training partner and a software architect who loves mobile and cloud";
 
         public IEnumerable<Uri> FeedUris
         {
