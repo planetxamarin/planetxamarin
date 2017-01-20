3c3a3f29cf1271826bcbd0dabffe7baaab481acc
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-01-20 16:56:56 +1100

    Update PeterFoot.cs

--- src/Firehose.Web/Authors/PeterFoot.cs
+++ src/Firehose.Web/Authors/PeterFoot.cs
@@ -10,7 +10,6 @@ namespace Firehose.Web.Authors
 	{
 		public string FirstName => "Peter";
 		public string LastName => "Foot";
-		public string Title => "Xamarin and Windows developer at In The Hand Ltd";
 		public string StateOrRegion => "United Kingdom";
 		public string EmailAddress => "peter@peterfoot.net";
 		public string TwitterHandle => "peterfoot";
@@ -20,14 +19,14 @@ namespace Firehose.Web.Authors
         
 		public string GravatarHash => "fa15aeeccc4b23e8a4677aeacb65b7bb";
 
-        public string ShortBioOrTagLine => "Peter Foot is a Xamarin and Windows developer at In The Hand Ltd";
+                public string ShortBioOrTagLine => "develops Xamarin and Windows applications at In The Hand Ltd";
 
-        public string GitHubHandle => "peterfoot";
+                public string GitHubHandle => "peterfoot";
 
 
-        public GeoPosition Position => new GeoPosition(52.76872, -2.37825);
+                public GeoPosition Position => new GeoPosition(52.76872, -2.37825);
 
-        public bool Filter(SyndicationItem item)
+                public bool Filter(SyndicationItem item)
 		{
 			
 			return item.Categories.Where(i => i.Name.Equals("Xamarin", StringComparison.OrdinalIgnoreCase)).Any();
