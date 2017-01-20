ac5c09637e96729dccf0005a26b2c161b6bc04ee
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2017-01-21 00:12:37 +0100

    Mobileprogrammer blog added. (#146)
    * Create DanielKrzyczkowski.cs
    
    * Update DanielKrzyczkowski.cs
    
    * Update with csproj with DanielKrzyczkowski.cs
    
    * Description updated.
    
    * Updated Filter method.
    
    * Add using statements
    
    * IAmAMicrosoft MVP does not implement FirstAwarded anymore.

---
+++ src/Firehose.Web/Authors/DanielKrzyczkowski.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class DanielKrzyczkowski : IAmAMicrosoftMVP, IFilterMyBlogPosts
+    {
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("http://mobileprogrammerblog.wordpress.com/feed/rss"); }
+        }
+
+        public string FirstName => "Daniel";
+        public string LastName => "Krzyczkowski";
+        public string StateOrRegion => "Warsaw, Poland";
+        public string EmailAddress => "daniel.krzyczkowski@hotmail.com";
+        public string ShortBioOrTagLine => "Focused on mobile world. He is passionate about mobile applications and devices. He loves learning new things about mobile development. Main part of these activities is to teach others about .NET Framework, C# programming language, Universal Windows Platform and Xamarin.";
+        public Uri WebSite => new Uri("http://mobileprogrammer.pl");
+        public string TwitterHandle => "DKrzyczkowski";
+        public string GitHubHandle => "Daniel-Krzyczkowski";
+        public string GravatarHash => "65e521a756dff3ac3c233e58afc9ee2b";
+        public GeoPosition Position => new GeoPosition(52.230878, 21.010842);
+        
+          public bool Filter(SyndicationItem item)
+          {
+            return item.Title.Text.ToLowerInvariant().Contains("xamarin") && item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
+          }
+    }
+}
