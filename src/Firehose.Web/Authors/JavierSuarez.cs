d126d84fa9828d1ad4347a5cc442cdfa131664a3
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-01-20 19:36:14 +0100

    Updated profile, added bio, github and Xamarin MVP.

--- src/Firehose.Web/Authors/JavierSuarez.cs
+++ src/Firehose.Web/Authors/JavierSuarez.cs
@@ -6,7 +6,7 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class JavierSuarez : IAmAMicrosoftMVP, IFilterMyBlogPosts
+    public class JavierSuarez : IAmAMicrosoftMVP, IAmAXamarinMVP, IFilterMyBlogPosts
     {
         public IEnumerable<Uri> FeedUris
         {
@@ -17,13 +17,12 @@ namespace Firehose.Web.Authors
         public string LastName => "Suarez";
         public string StateOrRegion => "Seville, Spain";
         public string EmailAddress => "javiersuarezruiz@hotmail.com";
-        public string ShortBioOrTagLine => string.Empty;
+        public string ShortBioOrTagLine => "is a passionate software developer from Spain who enjoys learning, talk and help others";
         public Uri WebSite => new Uri("http://javiersuarezruiz.wordpress.com");
         public string TwitterHandle => "jsuarezruiz";
         public string GravatarHash => "";
         public GeoPosition Position => new GeoPosition(37.3890920, -5.9844590);
-
-        public string GitHubHandle => string.Empty;
+        public string GitHubHandle => "jsuarezruiz";
 
         public bool Filter(SyndicationItem item)
         {
