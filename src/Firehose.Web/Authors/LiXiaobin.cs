91ed2234ebd0b1be5bc041c7371a0a5dfdee8828
li xiaobin <NewBLifes@hotmail.com>
2017-01-20 14:49:19 +0900

    add LiXiaobin bolg site

---
+++ src/Firehose.Web/Authors/LiXiaobin.cs
@@ -0,0 +1,22 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+    public class LiXiaobin : IAmACommunityMember
+    {
+        public string FirstName => "Li";
+        public string LastName => "Xiaobin";
+        public string ShortBioOrTagLine => "Xamarin.Froms and UWP";
+        public string StateOrRegion => "Jiangxi";
+        public string EmailAddress => "newblifes@hotmail.com";
+        public string TwitterHandle => "newblifes";
+        public string GravatarHash => string.Empty;
+        public string GitHubHandle => "NewBLife";
+        public GeoPosition Position => new GeoPosition(27.424025, 114.552699);
+        public Uri WebSite => new Uri("http://www.cnblogs.com/lixiaobin/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://feed.cnblogs.com/blog/u/93829/rss"); } }
+
+    }
+}
