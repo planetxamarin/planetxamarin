3a5d64ebabb028ab7891aeaf32749ee2903cd2b5
Matthew Robbins <matthew.ch.robbins@gmail.com>
2017-03-02 14:37:35 -0500

    Add MFractor as a feed author

---
+++ src/Firehose.Web/Authors/MFractor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class MFractor : IAmACommunityMember
+    {
+        public string FirstName => "MFractor";
+        public string LastName => "";
+        public string StateOrRegion => "Sydney, Australia";
+        public string EmailAddress => "hello@mfractor.com";
+        public string ShortBioOrTagLine => "is a collection of incredible tools for Xamarin Studio.";
+        public Uri WebSite => new Uri("https://www.mfractor.com/");
+        public string TwitterHandle => "mfractor";
+        public string GitHubHandle => "mfractor";
+        public string GravatarHash => "35bac056166a67222ddcd48b57113a32";
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://www.mfractor.com/blogs/news.atom"); }
+        }
+        public GeoPosition Position => new GeoPosition(-33.8678500, 151.2073200);
+    }
+}
