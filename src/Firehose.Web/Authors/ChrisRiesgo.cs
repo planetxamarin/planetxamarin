970cee2df3b7a8598a8a2300b2f68653d731c510
Chris Riesgo <cmriesgo@gmail.com>
2017-03-03 04:48:19 -0600

    Added @chrisriesgo back (#232)

---
+++ src/Firehose.Web/Authors/ChrisRiesgo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web
+{
+	public class ChrisRiesgo : IAmAMicrosoftMVP, IAmAXamarinMVP, IFilterMyBlogPosts
+	{
+		public string FirstName => "Chris";
+		public string LastName => "Riesgo";
+		public string ShortBioOrTagLine => "uses machines to help make the world a more magical place.";
+		public string EmailAddress => "";
+		public string StateOrRegion => "Nashville, TN";
+		public GeoPosition Position => new GeoPosition(36.174465, -86.767960);
+
+		public string TwitterHandle => "chrisriesgo";
+		public string GravatarHash => "f2164f68df9d2592894bd9c8fdb758bc";
+		public string GitHubHandle => "chrisriesgo";
+
+		public Uri WebSite => new Uri("http://chrisriesgo.com");
+
+		public IEnumerable<Uri> FeedUris
+		{
+			get { yield return new Uri("http://chrisriesgo.com/rss/"); }
+		}
+
+		public bool Filter(SyndicationItem item) =>
+			item.Title.Text.ToLowerInvariant().Contains("xamarin") ||
+			item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
+	}
+}
