0e3e4febf7fb27e16b3407885402286c83858ac7
Amjad <amjad.alhusseini@aramex.com>
2017-03-03 17:28:30 +0200

    Added GitHubHandle and GeoPosition properties

--- src/Firehose.Web/Authors/AmjadHusseini.cs
+++ src/Firehose.Web/Authors/AmjadHusseini.cs
@@ -13,6 +13,8 @@ namespace Firehose.Web.Authors
     public string EmailAddress => "amjad_h_cool@hotmail.com";
     public string TwitterHandle => "Amjad_Husseini";
     public string GravatarHash => "7eb71ff7195835154692f3fe61a0dfa8";
+    public string GitHubHandle => "amjad-Alhusseini";
+    public GeoPosition Position => new GeoPosition(31.973531, 35.860334);
 
     public Uri WebSite => new Uri("https://amjadhusseini.com/");
     public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://amjadhusseini.com/feed"); } }
