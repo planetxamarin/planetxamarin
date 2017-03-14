d0788f010b39f2fc2f732c364a80c314de22c50b
Matt Soucoup <msoucoup@codemilltech.com>
2017-03-14 11:51:45 -0500

    Updating GitHub handle & filter

--- src/Firehose.Web/Authors/MatthewSoucoup.cs
+++ src/Firehose.Web/Authors/MatthewSoucoup.cs
@@ -20,7 +20,10 @@ namespace Firehose.Web.Authors
         public string TwitterHandle => "codemillmatt";
         public string GravatarHash => "df69069a0bffd2dae5a8700a1bef7bfd";
 
-        public string GitHubHandle => string.Empty;
+        public string GitHubHandle => "codemillmatt";
         public GeoPosition Position => new GeoPosition(43.0730520, -89.4012300);
+
+        public bool Filter(SyndicationItem item) => 
+            item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
     }
 }
