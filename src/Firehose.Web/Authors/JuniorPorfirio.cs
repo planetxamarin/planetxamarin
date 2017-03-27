571b0ba96d71b7a770a6784719ce61561c5bc2d5
Junior Porfirio <juniorporfirio@gmail.com>
2017-03-27 14:43:36 -0300

    Filter add for profile author
    Filter add for profile author

--- src/Firehose.Web/Authors/JuniorPorfirio.cs
+++ src/Firehose.Web/Authors/JuniorPorfirio.cs
@@ -1,37 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ServiceModel.Syndication;
 using Firehose.Web.Infrastructure;
+using System.Linq;
 
 namespace Firehose.Web
 {
-	public class JuniorPorfirio : IAmACommunityMember
-	{
-
-		public string EmailAddress => "";
-
-		public IEnumerable<Uri> FeedUris
-		{
-			get { yield return new Uri("http://www.juniorporfirio.com.br/rss"); }
-		}
-
+	public class JuniorPorfirio : IAmACommunityMember, IFilterMyBlogPosts
+    {
 		public string FirstName => "Junior";
-
 		public string GitHubHandle => "juniorporfirio";
-
 		public string GravatarHash => "cc6a0796549f7eab6d3db20a3afa3148";
-
 		public string LastName => "Porfirio";
-
 		public GeoPosition Position => new GeoPosition(-22.907104, -47.063240);
-
 		public string ShortBioOrTagLine => "loves develop codes C# and talk /write about Xamarin";
-
-		public string StateOrRegion => "São Paulo, Brasil";
-
+		public string StateOrRegion => "Campinas, Brasil";
 		public string TwitterHandle => "juniorporfirio";
-
 		public Uri WebSite => new Uri("http://www.juniorporfirio.com.br");
-
-
-	}
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://www.juniorporfirio.com.br/rss"); } }
+        public bool Filter(SyndicationItem item) => item.Title.Text.Contains("xamarin") || item.Categories.Any(c => c.Name.ToLowerInvariant().Contains("xamarin"));
+    }
 }
