328f0dff8d614e6e9625cf4c949118ede923690e
mindOfAi <bryananthonygarcia@live.com>
2017-03-30 20:33:12 +0800

    Added BryanAnthonyGarcia.cs to the authors

--- src/Firehose.Web/Authors/BryanAnthonyGarcia.cs
+++ src/Firehose.Web/Authors/BryanAnthonyGarcia.cs
@@ -8,7 +8,7 @@
         public string TwitterHandle => "mindofai";
         public string EmailAddress => "bryananthonygarcia@live.com";
         public string ShortBioOrTagLine => "Bryan Anthony Garcia is a Mobile .NET Developer who enjoys learning and writing about Mobile .NET stuff and has passion for basketball and football. Co-leads Xamarin Mobile Developers - Philippines";
-        public string GravatarHash => "29f7fb03af5c354d6098f0300114056b";
+        public string GravatarHash => "29e1cdab06c48322805220e33556c20c";
         public Uri WebSite => new Uri("https://mindofai.github.io/");
 
         public IEnumerable<Uri> FeedUris
