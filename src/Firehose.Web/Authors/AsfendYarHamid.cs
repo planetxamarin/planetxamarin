5b49131ce117a7cdf45c6ecd90c683a0e6ae852f
asfend <asfend@hotmail.com>
2017-05-15 01:12:00 -0700

    Update AsfendYarHamid.cs (#264)
    * Update AsfendYarHamid.cs
    
    * Update AsfendYarHamid.cs

--- src/Firehose.Web/Authors/AsfendYarHamid.cs
+++ src/Firehose.Web/Authors/AsfendYarHamid.cs
@@ -14,7 +14,7 @@ namespace Firehose.Web.Authors
 
         public string EmailAddress => "asfend@hotmail.com";
 
-        public string ShortBioOrTagLine => "Passionate Software Engineer.";
+        public string ShortBioOrTagLine => "is a Technical Trainer, Author at Udemy and Passionate Community Member as well as Software Engineer";
 
         public Uri WebSite => new Uri("https://xamarinui.blogspot.com/");
 
@@ -25,7 +25,7 @@ namespace Firehose.Web.Authors
 
         public string TwitterHandle => "asfend";
 
-        public string GravatarHash => "";
+        public string GravatarHash => "1aa9a7436eec5ad5d0418a385d1bdbe0";
         public string GitHubHandle => "asfend";
         public GeoPosition Position => new GeoPosition(31.5712, 74.3646);
     }
