223c36cbce2ca5fe46ae9eaa65513eb98ee76d31
Damian Mehers <damian@mehers.com>
2017-06-11 18:48:46 +0200

    Added Gravatar hash and filter

--- src/Firehose.Web/Authors/DamianMehers.cs
+++ src/Firehose.Web/Authors/DamianMehers.cs
@@ -1,4 +1,4 @@
-public class DamianMehers : IAmACommunityMember
+public class DamianMehers : IAmACommunityMember, IFilterMyBlogPosts
 {
     public string FirstName => "Damian";
     public string LastName => "Mehers";
@@ -6,10 +6,15 @@ public class DamianMehers : IAmACommunityMember
     public string StateOrRegion => "Geneva, Switzerland";
     public string EmailAddress => "damian@mehers.com";
     public string TwitterHandle => "DamianMehers";
-    public string GravatarHash => "42abc1337def";
+    public string GravatarHash => "d77613f4e20bfcae401a6bf0018a83d1";
     public string GitHubHandle => "DamianMehers";
     public GeoPosition Position => new GeoPosition(46.3635288,6.1860801);
 
     public Uri WebSite => new Uri("https://damianblog.com.com/");
     public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://damianblog.com/feed/"); } }
+    
+    public bool Filter(SyndicationItem item)
+    {
+        return item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
+    }
 }
