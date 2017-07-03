972db7c158eaf2e93c2e19dca9a5a020021856c5
Ashan Dhanapala <dam@tiqri.com>
2017-07-03 16:24:05 +0530

    Added Ashan Dhanapala as a author

---
+++ src/Firehose.Web/Authors/AshanDhanapala.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class AshanDhanapala : IAmACommunityMember
+    {
+        public string FirstName => "Ashan";
+        public string LastName => "Dhanapala";
+        public string ShortBioOrTagLine => "Wannabe serious guy and Xamarin enthusiast";
+        public string StateOrRegion => "Sri Lanka";
+        public string EmailAddress => "ashandhanapala@gmail.com";
+        public string TwitterHandle => "ashanCodes";
+        public string GravatarHash => "fabf988b09d5dae70fb8545ca07c879e";
+        public string GitHubHandle => "ashancodes";
+        public GeoPosition Position => new GeoPosition(6.9271, 79.8612);
+
+        public Uri WebSite => new Uri("http://ashancodes.blog/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://ashancodes.blog/rss"); } }
+    }
+}
