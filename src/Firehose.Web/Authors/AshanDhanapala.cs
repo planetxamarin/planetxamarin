d2f4e81577470dfee060a008b2f0f9f3d0ed12c4
Ashan Dhanapala <dam@tiqri.com>
2017-07-03 16:29:27 +0530

    Added the filer

--- src/Firehose.Web/Authors/AshanDhanapala.cs
+++ src/Firehose.Web/Authors/AshanDhanapala.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
 using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class AshanDhanapala : IAmACommunityMember
-    {
+    public class AshanDhanapala : IAmACommunityMember,IFilterMyBlogPosts
+	{
         public string FirstName => "Ashan";
         public string LastName => "Dhanapala";
         public string ShortBioOrTagLine => "Wannabe serious guy and Xamarin enthusiast";
@@ -18,5 +20,13 @@ namespace Firehose.Web.Authors
 
         public Uri WebSite => new Uri("http://ashancodes.blog/");
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://ashancodes.blog/rss"); } }
+
+        public bool Filter(SyndicationItem item)
+        {
+            
+			// Of course you can make the checks as complicated as you want and combine some stuff
+            return item.Title.Text.ToLowerInvariant().Contains("xamarin") && item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
+
+		}
     }
 }
