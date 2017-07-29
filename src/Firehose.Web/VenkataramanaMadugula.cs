0b490c0b433dec62d4d917b90db71b8cb6ea446e
Venkataramana Madugula <madugula.venkataramana@gmail.com>
2017-07-29 23:24:03 +0530

    Create VenkataramanaMadugula.cs file
    I would like to add my blog posts related to Xamarin to get added to Planet Xamarin Community. Kindly approve this request.

---
+++ src/Firehose.Web/VenkataramanaMadugula.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class AlexDunn : IAmACommunityMember
+    {
+        public string FirstName => "Venkataramana";
+        public string LastName => "Madugula";
+        public string StateOrRegion => "Hyderabad, India";
+        public string TwitterHandle => "madugula83";
+        public string EmailAddress => "madugula.venkataramana@gmail.com";
+        public string ShortBioOrTagLine => "Xamarin Certified Developer.";
+        public string GravatarHash => "a197aa8d40cd9aa859be8642c5b38123";
+        public Uri WebSite => new Uri("https://www.madugulavenkataramana.com");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("http://www.madugulavenkataramana.com/feed/"); }
+        }
+
+        public string GitHubHandle => "VenkatsQuest";
+
+        public bool Filter(SyndicationItem item) =>
+            item.Title.Text.ToLowerInvariant().Contains("xamarin") ||
+            item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
+        public GeoPosition Position => new GeoPosition(17.387140, 78.491684);
+    }
+}
