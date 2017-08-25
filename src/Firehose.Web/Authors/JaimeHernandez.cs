ae41e91682ad5d49ce4467b5eb242eea449d92bc
Jaime Hernández <jhernandez.hs@icloud.com>
2017-08-24 23:02:44 -0300

    actualizar
    actualizar

--- src/Firehose.Web/Authors/JaimeHernandez.cs
+++ src/Firehose.Web/Authors/JaimeHernandez.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
@@ -15,15 +16,13 @@ namespace Firehose.Web.Authors
         public string ShortBioOrTagLine => "Systems analyst developing web systems and mobile applications" ;
         public string GravatarHash => "35939afef20aac1680fc71bbd6d9c214";
         public Uri WebSite => new Uri("http://lawebdelprogramador.cl/bloglawebdelprogramador/");
-
-        public IEnumerable<Uri> FeedUris
-        {
+        public IEnumerable<Uri> FeedUris{
             get { yield return new Uri("http://lawebdelprogramador.cl/bloglawebdelprogramador/feed/"); }
         }
         public string GitHubHandle => "lawebdeprogramador";
         public GeoPosition Position => new GeoPosition(-33.4488897, -70.6692655);
         public bool Filter(SyndicationItem item){
-		return item.Title.Text.ToLowerInvariant().Contains("xamarin");
-	}
+			return item.Title.Text.ToLowerInvariant().Contains("xamarin");
+		}
     }
 }
