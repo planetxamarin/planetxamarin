d9f5017dfa5724f78cfa1b8a9c5bbc953e01d177
Sven-Michael Stübe <stuebe2k7@gmail.com>
2017-09-17 18:46:13 +0200

    Sven-Michael Stübe HTTPS update

--- src/Firehose.Web/Authors/SvenMichaelStuebe.cs
+++ src/Firehose.Web/Authors/SvenMichaelStuebe.cs
@@ -6,23 +6,23 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class SvenMichaelStuebe : IAmAMicrosoftMVP, IFilterMyBlogPosts
+    public class SvenMichaelStuebe : IAmAMicrosoftMVP, IAmAXamarinMVP, IFilterMyBlogPosts
     {
         public string FirstName => "Sven-Michael";
         public string LastName => "Stübe";
-        public string ShortBioOrTagLine => "";
+        public string ShortBioOrTagLine => "loves the cross plattform approach and develops some Xamarin plugins. He organizes the Xamarin Pizza & Beer Meetups in Munich.";
         public string StateOrRegion => "Munich, Germany";
         public string EmailAddress => string.Empty;
         public string TwitterHandle => "stuebe2k14";
 
-        public Uri WebSite => new Uri("http://smstuebe.de/");
+        public Uri WebSite => new Uri("https://smstuebe.de/");
 
         public IEnumerable<Uri> FeedUris
         {
-            get { yield return new Uri("http://smstuebe.de/feed.xml"); }
+            get { yield return new Uri("https://smstuebe.de/feed.xml"); }
         }
 
-        public string GitHubHandle => string.Empty;
+        public string GitHubHandle => "smstuebe";
         public string GravatarHash => "08b73d0a58fc120a8cc8dc561d83b3d6";
         public GeoPosition Position => new GeoPosition(48.1373831, 11.5063151);
 
