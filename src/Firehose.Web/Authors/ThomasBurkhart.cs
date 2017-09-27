68122482a65802ca677455d2dce00d36232a2ae9
escamoteur <github@burkharts.net>
2017-09-27 19:16:58 +0200

    updated to SSL

--- src/Firehose.Web/Authors/ThomasBurkhart.cs
+++ src/Firehose.Web/Authors/ThomasBurkhart.cs
@@ -13,13 +13,13 @@ namespace Firehose.Web.Authors
         public string StateOrRegion => "Bonn, Germany";
         public string EmailAddress => "blog@burkharts.net";
         public string ShortBioOrTagLine => "Indie App developer and freelancer; Xamarin Forms Evangelist ;-)";
-        public Uri WebSite => new Uri("http://blog.burkharts.net/");
+        public Uri WebSite => new Uri("https://blog.burkharts.net/");
         public string TwitterHandle => "ThomasBurkhartB";
         public string GitHubHandle => "escamoteur";
         public string GravatarHash => "e84ea2a58f302e42497e352212bfe5e5";
         public IEnumerable<Uri> FeedUris
         {
-            get { yield return new Uri("http://www.burkharts.net/apps/blog/feed/"); }
+            get { yield return new Uri("https://www.burkharts.net/apps/blog/feed/"); }
         }
         public GeoPosition Position => new GeoPosition(50.687586, 7.149718);
     }
