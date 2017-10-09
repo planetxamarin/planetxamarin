c76b69132289869da2495342155261b051c03b1e
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-10-08 18:29:56 -0700

    Update Robin-Manuel Thiel's Twitter Handle

--- src/Firehose.Web/Authors/RobinManuelThiel.cs
+++ src/Firehose.Web/Authors/RobinManuelThiel.cs
@@ -16,10 +16,10 @@ namespace Firehose.Web.Authors
 
         public string FirstName => "Robin-Manuel";
         public string LastName => "Thiel";
-        public string ShortBioOrTagLine => "Technical Evangelist at Microsoft by day, tinker and fiddler by night. Loves everything with a power plug or IP address.";
+        public string ShortBioOrTagLine => "Techie at Microsoft by day, tinker and fiddler by night. Loves everything with a power plug or IP address.";
         public string StateOrRegion => "Munich, Germany";
         public string EmailAddress => string.Empty;
-        public string TwitterHandle => "einRobby";
+        public string TwitterHandle => "robinmanuelt";
         public string GitHubHandle => "robinmanuelthiel";
         public GeoPosition Position => new GeoPosition(48.177622, 11.5912643);
         public Uri WebSite => new Uri("https://pumpingco.de/");
