f85ac240a73845cb1da90061473ae8fabfa4bda8
Marc Bruins <marciebruins@gmail.com>
2017-10-24 11:07:08 +0200

    Update MarcBruins.cs (#378)
    Update urls

--- src/Firehose.Web/Authors/MarcBruins.cs
+++ src/Firehose.Web/Authors/MarcBruins.cs
@@ -14,14 +14,14 @@ namespace Firehose.Web.Authors
         public string EmailAddress => "marc@marcbruins.nl";
         public string TwitterHandle => "MarcBruins";
         public string GravatarHash => "3795d2031be87499f76f6336ec5a3a45";
-        public string StateOrRegion => "Groningen, Netherlands";
-        public Uri WebSite => new Uri("http://www.marcbruins.nl");
+        public string StateOrRegion => "Utrecht, Netherlands";
+        public Uri WebSite => new Uri("https://www.marcbruins.nl");
         public string GitHubHandle => "MarcBruins";
         public GeoPosition Position => new GeoPosition(53.2193840, 6.5665020);
 
         public IEnumerable<Uri> FeedUris
         {
-            get { yield return new Uri("http://www.marcbruins.nl/feed/"); }
+            get { yield return new Uri("https://www.marcbruins.nl/feed.xml"); }
         }
 
         public bool Filter(SyndicationItem item) =>
