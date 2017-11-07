695dc0bad6a9d95b9ab8e3b4415e4f6bd834f129
Mark Deanil Vicente <markdeanilvicente@gmail.com>
2017-11-07 17:21:13 +0800

    Update MarkDeanilVicente.cs

--- src/Firehose.Web/Authors/MarkDeanilVicente.cs
+++ src/Firehose.Web/Authors/MarkDeanilVicente.cs
@@ -12,7 +12,7 @@ namespace Firehose.Web.Authors
         public string LastName => "Vicente";
         public string StateOrRegion => "Manila, Philippines";
         public string EmailAddress => "markdeanilvicente@gmail.com";
-        public string ShortBioOrTagLine => "Mark Deanil Vicente is a Microsoft MVP. He's currently working as programmer & a developer. He's also one of the founders of Mobile .Net Developers PH (MondPH) Community. He enjoys drinking wine and hot tea";
+        public string ShortBioOrTagLine => "Microsoft MVP. Programmer and a developer. He enjoys drinking wine and hot tea";
         public Uri WebSite => new Uri("https://deanilvincent.github.io/");
         public string TwitterHandle => "DeanilVincent03";
         public string GitHubHandle => "deanilvincent";
