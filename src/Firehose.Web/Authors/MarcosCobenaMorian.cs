4514a9b45afab6e2814816b51aa2969a7dd833d9
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-01-30 10:41:42 +0100

    Added HTTPS domains

--- src/Firehose.Web/Authors/MarcosCobenaMorian.cs
+++ src/Firehose.Web/Authors/MarcosCobenaMorian.cs
@@ -10,7 +10,7 @@ namespace Firehose.Web.Authors
         public string LastName => "Cobeña Morián";
 		public string ShortBioOrTagLine => "I write apps for people using Xamarin";
         public string EmailAddress => "marcoscm.digital@gmail.com";
-		public Uri WebSite => new Uri("http://marcoscobena.com");
+		public Uri WebSite => new Uri("https://marcoscobena.com");
 
 		public string StateOrRegion => "Sevilla (Andalucía), Spain";
 		public GeoPosition Position => new GeoPosition(37.3753501, -6.0250978);
@@ -18,6 +18,6 @@ namespace Firehose.Web.Authors
 		public string GitHubHandle => "MarcosCobena";
 		public string TwitterHandle => "1Marcos2Cobena";
 
-        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://feeds.feedburner.com/MarcosCobena"); } }
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://marcoscobena.com/feed-Xamarin.rss"); } }
     }
 }
