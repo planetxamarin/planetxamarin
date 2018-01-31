fa3ceeb06aa12acc15b6b4693410f6b6e39c201a
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-01-31 10:05:36 +0100

    Fix indentation

--- src/Firehose.Web/Authors/MarcosCobenaMorian.cs
+++ src/Firehose.Web/Authors/MarcosCobenaMorian.cs
@@ -4,19 +4,19 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-	public class MarcosCobenaMorian
+    public class MarcosCobenaMorian
     {
         public string FirstName => "Marcos";
         public string LastName => "Cobeña Morián";
-		public string ShortBioOrTagLine => "I write apps for people using Xamarin";
+        public string ShortBioOrTagLine => "I write apps for people using Xamarin";
         public string EmailAddress => "marcoscm.digital@gmail.com";
-		public Uri WebSite => new Uri("https://marcoscobena.com");
+        public Uri WebSite => new Uri("https://marcoscobena.com");
 
-		public string StateOrRegion => "Sevilla (Andalucía), Spain";
-		public GeoPosition Position => new GeoPosition(37.3753501, -6.0250978);
+        public string StateOrRegion => "Sevilla (Andalucía), Spain";
+        public GeoPosition Position => new GeoPosition(37.3753501, -6.0250978);
         
-		public string GitHubHandle => "MarcosCobena";
-		public string TwitterHandle => "1Marcos2Cobena";
+        public string GitHubHandle => "MarcosCobena";
+        public string TwitterHandle => "1Marcos2Cobena";
 
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://marcoscobena.com/feed-Xamarin.rss"); } }
     }
