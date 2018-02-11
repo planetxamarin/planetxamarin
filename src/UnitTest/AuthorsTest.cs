d625f928dd8f2dfef27b883caf8d5ec8dedebd67
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-02-11 21:19:13 +0100

    Add tests to check scheme and hit the URL to check validity

--- src/UnitTest/AuthorsTest.cs
+++ src/UnitTest/AuthorsTest.cs
@@ -1,7 +1,14 @@
 ﻿using Firehose.Web.Infrastructure;
+using Polly;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Reflection;
+using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace UnitTest
 {
@@ -17,6 +24,21 @@ namespace UnitTest
             nameof(IAmANewsletter),
             nameof(IAmAFrameworkForXamarin)
         };
+
+        readonly ITestOutputHelper _output;
+
+        Policy _policy = Policy.Handle<WebException>(
+            ex => !ex.Message.Contains("Could not create SSL/TLS secure channel"))
+            .WaitAndRetryAsync(3, retryAttempt =>
+                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        static HttpClient _httpClient = new HttpClient();
+
+        public AuthorsTest(ITestOutputHelper output)
+        {
+            _output = output;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+        }
 
         [Fact]
         public void All_Authors_Implement_Interface()
@@ -40,14 +62,51 @@ namespace UnitTest
             var assembly = Assembly.GetAssembly(typeof(IAmACommunityMember));
 
             var types = assembly.GetTypes();
-            var authors = types.Where(t => typeof(IAmACommunityMember).IsAssignableFrom(t)).ToArray();
+            var authors = types.Where(t => typeof(IAmACommunityMember).IsAssignableFrom(t) && 
+                !_interfaceNames.Contains(t.Name)).ToArray();
 
             foreach(var author in authors)
             {
-                if (_interfaceNames.Contains(author.Name)) continue;
-
                 Assert.True(author.Namespace == "Firehose.Web.Authors",
                     $"{author.Name} is not in the correct namespace");
+            }
+        }
+
+        [Fact]
+        public async Task All_Authors_Have_Secure_Feed()
+        {
+            var assembly = Assembly.GetAssembly(typeof(IAmACommunityMember));
+
+            var types = assembly.GetTypes();
+            var authorTypes = types.Where(t => typeof(IAmACommunityMember).IsAssignableFrom(t) &&
+                !_interfaceNames.Contains(t.Name)).ToArray();
+
+            var hitFeedTasks = new List<Task>();
+            foreach(var authorType in authorTypes)
+            {
+                var author = (IAmACommunityMember)Activator.CreateInstance(authorType);
+                foreach(var feed in author.FeedUris)
+                {
+                    Assert.Equal("https", feed.Scheme);
+
+                    // hit the url to see if it responds!
+                    hitFeedTasks.Add(HitFeedAsync(feed));
+                }
+            }
+
+            await Task.WhenAll(hitFeedTasks).ConfigureAwait(false);
+        }
+
+        private async Task HitFeedAsync(Uri feedUrl)
+        {
+            try
+            {
+                await _policy.ExecuteAsync(() => _httpClient.GetAsync(feedUrl))
+                    .ConfigureAwait(false);
+            }
+            catch
+            {
+                _output.WriteLine($"{feedUrl} sucks...");
             }
         }
     }
