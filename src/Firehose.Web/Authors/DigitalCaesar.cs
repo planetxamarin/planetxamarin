4140507b47a9a0832116db54e8b6e25b4e7630ad
Eric DeCesare <eric.decesare@outlook.com>
2018-03-08 22:51:47 -0500

    Revert "Delete DigitalCaesar.cs"
    This reverts commit 8cfbba3bcddf56b3023ac698f16d2d2f863e4113.

---
+++ src/Firehose.Web/Authors/DigitalCaesar.cs
@@ -0,0 +1,15 @@
+public class DigitalCaesar : IAmACommunityMember 
+{ 
+    public string FirstName => "Tony"; 
+    public string LastName => "Caesar"; 
+    public string ShortBioOrTagLine => "Bringing the Digital World to Life"; 
+    public string StateOrRegion => "USA"; 
+    public string EmailAddress => "tony@digitalcaesar.com"; 
+    public string TwitterHandle => "Digital_Caesar"; 
+    public string GravatarHash => "7c63b2b2254e3b0abf896577ab2746aa"; 
+    public string GitHubHandle => "digitalcaesar"; 
+    public GeoPosition Position => new GeoPosition(47.643417, -122.126083); 
+    public Uri WebSite => new Uri("https://digitalcaesar.com/"); 
+    public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://digitalcaesar.com/rss"); } } 
+    public FeedLanguageCode => "en"; 
+} 
