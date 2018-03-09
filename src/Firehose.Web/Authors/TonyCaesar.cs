ffefc11cca669f864f6eb09b671f9b7daebfb44c
Digital Caesar LLC <eric@digitalcaesar.com>
2018-03-08 22:06:25 -0500

    Update TonyCaesar.cs

--- src/Firehose.Web/Authors/TonyCaesar.cs
+++ src/Firehose.Web/Authors/TonyCaesar.cs
@@ -1,5 +1,11 @@
-public class TonyCaesar : IAmACommunityMember 
-{ 
+using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+  public class TonyCaesar : IAmACommunityMember 
+  { 
     public string FirstName => "Tony"; 
     public string LastName => "Caesar"; 
     public string ShortBioOrTagLine => "Bringing the Digital World to Life"; 
@@ -8,8 +14,9 @@ public class TonyCaesar : IAmACommunityMember
     public string TwitterHandle => "Digital_Caesar"; 
     public string GravatarHash => "7c63b2b2254e3b0abf896577ab2746aa"; 
     public string GitHubHandle => "digitalcaesar"; 
-    public GeoPosition Position => new GeoPosition(47.643417, -122.126083); 
+    public GeoPosition Position => new GeoPosition(33.30186, -84.5541); 
     public Uri WebSite => new Uri("https://digitalcaesar.com/"); 
     public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://digitalcaesar.com/rss"); } } 
     public FeedLanguageCode => "en"; 
-} 
+  } 
+}
