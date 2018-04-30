3d00b02443cc62baba017821d1ac884dd56f120c
Jefferson Balivo <jefferson@balivo.com.br>
2018-04-30 18:28:33 -0300

    Code review (Mini Bio) and removing Filter

--- src/Firehose.Web/Authors/JeffersonBalivo.cs
+++ src/Firehose.Web/Authors/JeffersonBalivo.cs
@@ -1,25 +1,21 @@
 ﻿using Firehose.Web.Infrastructure;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel.Syndication;
 
 namespace Firehose.Web.Authors
 {
-    public class JeffersonBalivo : IAmACommunityMember, IFilterMyBlogPosts
+    public class JeffersonBalivo : IAmACommunityMember
     {
         public string FirstName => "Jefferson";
         public string LastName => "Balivo";
         public string StateOrRegion => "Jaú, São Paulo, Brazil";
         public string EmailAddress => "jefferson@balivo.com.br";
-        public string ShortBioOrTagLine => "don't work, \"have fun\" with .NET. I'm \"Line of Business\" software (Desktop, Web and Mobile) specialist. Xamarin enthusiats since was just \"Mono4Android\" and \"MonoTouch\" (~2009).";
+        public string ShortBioOrTagLine => "Xamarin Enthusiast, Technical Lead and Cross Platform Architect, Multi-Plataform Technical Audience Contributor (MTAC), Technical Writer and Speaker";
         public Uri WebSite => new Uri("https://balivo.com.br/");
         public string TwitterHandle => "jbalivo";
         public string GitHubHandle => "balivo";
         public string GravatarHash => string.Empty;
         public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://balivo.com.br/rss"); } }
         public GeoPosition Position => new GeoPosition(-22.2997067, -48.5931324);
-
-        public bool Filter(SyndicationItem item) => item.Title.Text.ToLowerInvariant().Contains("xamarin") && item.Categories.Any(c => c.Name.ToLowerInvariant().Equals("xamarin"));
     }
 }
