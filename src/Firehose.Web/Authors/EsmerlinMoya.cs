1c79ae792d7eb2b1e6cb58e89d91db737445a655
Esmerlin Moya <Esmerlinmoya@gmail.com>
2018-05-10 11:12:16 -0400

    New Author: Esmerlin Moya

---
+++ src/Firehose.Web/Authors/EsmerlinMoya.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+	public class EsmerlinMoya : IAmACommunityMember
+    {
+		public string FirstName => "Esmerlin";
+        public string LastName => "Rosario Moya";
+		public string StateOrRegion => "Dominican Republic";
+		public string EmailAddress => "esmerlinmoya@gmail.com";
+		public string ShortBioOrTagLine => "Software and Mobile Developer";
+		public GeoPosition Position => new GeoPosition(18.503112, -69.889555);
+		public string TwitterHandle => "merlin1827";
+		public string GitHubHandle => "esmerlinmoya";
+		public string GravatarHash => "7a6f9f3adcdd5eb3737bce3e52719997";
+
+		public Uri WebSite => new Uri("https://medium.com/@esmerlinmoya");
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://medium.com/feed/@esmerlinmoya"); } }
+		public string FeedLanguageCode => "en";
+	}
+}
