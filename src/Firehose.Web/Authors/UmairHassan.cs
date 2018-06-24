f8b71941a0d2a2ae89e07ed4cb0819d1ef9d00ed
umair03 <37195067+umair03@users.noreply.github.com>
2018-06-24 19:48:08 +0500

    Create UmairHassan.cs

---
+++ src/Firehose.Web/Authors/UmairHassan.cs
@@ -0,0 +1,15 @@
+public class UmairHassan : IAmACommunityMember
+{
+    public string FirstName => "Umair";
+    public string LastName => "Hassan";
+    public string ShortBioOrTagLine => "Xamarin Developer, MVP at C-SharpCorner, Owner & Writer at XamarinWeb.Com";
+    public string StateOrRegion => "Lahore, Pakistan";
+    public string EmailAddress => "umair.hassan03@gmail.com";
+    public string TwitterHandle => "umairhassan03";
+    public string GravatarHash => "ecc4a16874f8078faf13bb6b8e0c90db";
+    public string GitHubHandle => "umair03";
+    public GeoPosition Position => new GeoPosition(31.582045, 74.329376);
+    public Uri WebSite => new Uri("http://xamarinweb.com/");
+    public IEnumerable<Uri> FeedUris { get { yield return new Uri("http://xamarinweb.com/blog/feed"); } }
+    public FeedLanguageCode => "en";
+}
