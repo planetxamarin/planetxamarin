8324a081c5ebaf80b41afeb0590f18e3988e0f8a
James Montemagno <james.montemagno@gmail.com>
2018-08-22 14:26:39 -0700

    Check for podcast keywords

--- src/Firehose.Web/Extensions/SyndicationItemExtensions.cs
+++ src/Firehose.Web/Extensions/SyndicationItemExtensions.cs
@@ -12,6 +12,15 @@ namespace Firehose.Web.Extensions
             if (item.Categories.Count > 0)
                 hasXamarinCategory = item.Categories.Any(category =>
                     category.Name.ToLowerInvariant().Contains("xamarin"));
+            else if(item.ElementExtensions.Count > 0)
+            {
+                var element = item.ElementExtensions.Where(e => e.OuterName == "keywords").FirstOrDefault();
+                if(element != null)
+                {
+                    var keywords = element.GetObject<string>();
+                    hasXamarinCategory = keywords.ToLowerInvariant().Contains("xamarin");
+                }
+            }
 
             var hasXamarinTitle = item.Title?.Text.ToLowerInvariant().Contains("xamarin") ?? false;
 
