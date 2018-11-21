72eddc3cc20aad47203800c72c99c40da4d54865
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-11-21 00:57:03 -0200

    Update info author Juliano Custódio

--- src/Firehose.Web/Authors/JulianoCustodio.cs
+++ src/Firehose.Web/Authors/JulianoCustodio.cs
@@ -4,17 +4,17 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class JulianoCustodio : IAmACommunityMember
+    public class JulianoCustodio : IAmAMicrosoftMVP
     {
         public string FirstName => "Juliano";
-        public string LastName => "Custódio";
-        public string StateOrRegion => "Sorocaba, Brasil";
+        public string LastName => "Custóio";
+        public string StateOrRegion => "Piracicaba, Brasil";
         public string TwitterHandle => "JuuCustodio";
         public string GitHubHandle => "JuuCustodio";
-        public string ShortBioOrTagLine => "Mobile developer";
+        public string ShortBioOrTagLine => "Microsoft MVP, Blogger and Speaker";
         public string EmailAddress => "juliano.custodio@hotmail.com.br";
         public string GravatarHash => "71de9936f2ffbc93e9918066479331f1";
-        public GeoPosition Position => new GeoPosition(-23.52425, -47.46501);
+        public GeoPosition Position => new GeoPosition(-22.7342864, -47.6480644);
         public Uri WebSite => new Uri("https://www.julianocustodio.com");
 
         public IEnumerable<Uri> FeedUris
