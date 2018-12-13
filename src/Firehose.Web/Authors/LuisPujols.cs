cd14932bb9da4c6d2b6a24494c8bbd4cad4ded30
Luis Pujols <luispujols@hotmail.com>
2018-12-12 21:34:57 -0400

    Added Luis Pujols as author

---
+++ src/Firehose.Web/Authors/LuisPujols.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class LuisPujols : IAmACommunityMember, IFilterMyBlogPosts
+    {
+        public string FirstName => "Luis";
+        public string LastName => "Pujols";
+        public string StateOrRegion => "United States";
+        public string EmailAddress => "luispujolso@gmail.com";
+        public string ShortBioOrTagLine => "Software Engineer";
+        public Uri WebSite => new Uri("https://www.pujolsluis.com/");
+        public string TwitterHandle => "Pujolsluis1";
+        public string GitHubHandle => "Pujolsluis";
+        public string GravatarHash => "c91c0d654f4f06ca6e7a7e54699de85d";
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://www.pujolsluis.com/rss"); }
+        }
+        public GeoPosition Position => new GeoPosition(26.0203048, -80.115093);
+        public string FeedLanguageCode => "en";
+
+        public bool Filter(SyndicationItem item)
+        {
+            // This filters out only the posts that have the "xamarin" word on
+            // their title or as a category
+            return item.Title.Text.ToLowerInvariant().Contains("xamarin")
+            || item.Categories.Any(category => category.Name.ToLowerInvariant().Contains("xamarin"));
+        }
+    }
+}
