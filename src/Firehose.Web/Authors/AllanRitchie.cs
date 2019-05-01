ac84f6327ef1f1ef4198310b93d6ad61a9c0b398
Allan Ritchie <allan.ritchie@gmail.com>
2019-05-01 03:06:33 -0400

    Adding Allan Ritchie as Author (#576)
    * adding author
    
    * add gravatar

---
+++ src/Firehose.Web/Authors/AllanRitchie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+
+namespace Firehose.Web.Authors
+{
+    public class AllanRitchie : IAmAXamarinMVP, IAmAMicrosoftMVP
+    {
+        public string FirstName => "Allan";
+
+        public string LastName => "Ritchie";
+
+        public string StateOrRegion => "Toronto, Canada";
+
+        public string EmailAddress => "allan.ritchie@gmail.com";
+
+        public string ShortBioOrTagLine => "";
+
+        public Uri WebSite => new Uri("https://allancritchie.net");
+
+        public string TwitterHandle => "allanritchie911";
+
+        public string GitHubHandle => "aritchie";
+
+        public string GravatarHash => "5f22bd04ca38ed4d0a5225d0825e0726";
+
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://allancritchie.net/xamarin.rss"); } }
+
+        public GeoPosition Position => new GeoPosition(43.6425701,-79.3892455);
+
+        public string FeedLanguageCode => "en";
+    }
+}
