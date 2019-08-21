f18e5854662060cdf6e1d921b9358cc2e5e1a900
Achraf ben alaya <achrafbenalaya@users.noreply.github.com>
2019-08-21 12:14:02 +0100

    fix missing ;

--- src/Firehose.Web/Authors/AchrafBenAlaya.cs
+++ src/Firehose.Web/Authors/AchrafBenAlaya.cs
@@ -2,7 +2,7 @@ using Firehose.Web.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel.Syndication
+using System.ServiceModel.Syndication;
 
 namespace Firehose.Web.Authors
 {
