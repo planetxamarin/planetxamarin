e3ab331caed725422e4c12ffa20d257dc31c9654
Trevor Tirrell <ttirrell@hotmail.com>
2019-10-29 13:20:09 -0400

    Added Trevor Tirrell as Author of ttirrell.com

---
+++ src/Firehose.Web/Authors/ttirrell.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+    public class ttirrell : IAmACommunityMember, IFilterMyBlogPosts
+    {
+        public string FirstName => "Trevor";
+        public string LastName => "Tirrell";
+        public string ShortBioOrTagLine => "is a Xamarin Certified Mobile Developer and Founder of ttirrell";
+        public string StateOrRegion => "Greenville, SC";
+        public string EmailAddress => "trevor@ttirrell.com";
+        public string TwitterHandle => "TrevorTirrell";
+        public string GravatarHash => "aa52960b7c71deb9ac4a18c62913842d";
+        public string GitHubHandle => "ttirrell";
+        public GeoPosition Position => new GeoPosition(34.86948, -82.404068);
+        public Uri WebSite => new Uri("https://ttirrell.com/");
+        public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://ttirrell.com/feed/"); } }
+        public string FeedLanguageCode => "en";
+
+        public bool Filter(SyndicationItem item)
+        {
+            return item.Categories?.Any(c => c.Name.ToLowerInvariant().Equals("xamarin")) ?? false;
+        }
+    }
+}
