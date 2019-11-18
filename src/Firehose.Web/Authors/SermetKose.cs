3598b5c60b43ad5f9cb1cfa10910b2d51d7f6ae4
Sermet KÖSE <sermet.kose@gmail.com>
2019-11-19 01:14:53 +0300

    Adding Sermet Kose as author.

---
+++ src/Firehose.Web/Authors/SermetKose.cs
@@ -0,0 +1,27 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+	public class SermetKose : IAmACommunityMember
+	{
+		public string FirstName => "Sermet";
+		public string LastName => "Kose";
+		public string StateOrRegion => "IST, Turkey";
+		public string EmailAddress => "sermetkose@gmail.com";
+		public string ShortBioOrTagLine => "is Software Developer at Apsiyon.";
+		public Uri WebSite => new Uri("https://medium.com/sermet-blog");
+
+		public IEnumerable<Uri> FeedUris
+		{
+			get { yield return new Uri("https://medium.com/feed/sermet-blog/tagged/P"); }
+		}
+
+		public string TwitterHandle => string.Empty;
+		public string GravatarHash => "5f0f99eb2cb09d1591079198f43418b9";
+		public string GitHubHandle => "sermetk";
+		public GeoPosition Position => new GeoPosition(41.0055005, 28.7319902);
+		public string FeedLanguageCode => "en";
+	}
+}
