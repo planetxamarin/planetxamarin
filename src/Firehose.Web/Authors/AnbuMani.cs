d6e19356a444a289d03134a4a83dcf5f0f850477
Anbu Mani <anbumani91@hotmail.com>
2019-12-18 13:04:37 +0000

    Added New Author

--- src/Firehose.Web/Authors/AnbuMani.cs
+++ src/Firehose.Web/Authors/AnbuMani.cs
@@ -6,13 +6,13 @@ public class AnbuMani : IAmACommunityMember
     {
         public string FirstName => "Anbu";
         public string LastName => "Mani";
-        public string ShortBioOrTagLine => "I'm Always love with Working in Community";
-        public string EmailAddress => string.Empty;
+        public string ShortBioOrTagLine => "Anbu Mani is a Microsoft MVP who Blogger, Speaker, Founder & Organizer of XMonkeys360 Community – India.";
+        public string EmailAddress => "anbumani@xmonkeys360.com";
         public string TwitterHandle => "anbu_mani27";
         public string GravatarHash => "f97650474e4aa5b9609a28dcfdb052d4";
         public string StateOrRegion => "Chennai, India";
         public Uri WebSite => new Uri("https://www.xmonkeys360.com/");
-        public GeoPosition Position => new GeoPosition(13.029217,80.2082503);
+        public GeoPosition Position => new GeoPosition(12.902749, 80.190846);
 
         public IEnumerable<Uri> FeedUris
         {
