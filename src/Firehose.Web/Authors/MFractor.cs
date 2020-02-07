e3caeb2010a992cbcb54aa43534427aa3bff4495
Matthew Robbins <matthew@mfractor.com>
2020-02-07 18:10:37 +1000

    Filter out Visual Studio extensibility blogs for MFractor. (#641)
    * Filter out Visual Studio extensibilty blogs for MFractor.
    
    * Fix missing usings.
    
    * Include System.Linq

--- src/Firehose.Web/Authors/MFractor.cs
+++ src/Firehose.Web/Authors/MFractor.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
 using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-    public class MFractor : IAmACommunityMember
+    public class MFractor : IAmACommunityMember, IFilterMyBlogPosts
     {
         public string FirstName => "MFractor";
         public string LastName => "";
@@ -29,5 +31,18 @@ namespace Firehose.Web.Authors
 
         public GeoPosition Position => new GeoPosition(-27.470125, 153.021072);
         public string FeedLanguageCode => "en";
+		
+		public bool Filter(SyndicationItem item)
+		{
+			// Filter out any topics that are for Visual Studio extensibility;
+			var isExtensibilityTopic = item.Categories?.Any(c => c.Name.ToLowerInvariant().Equals("visual studio extensibility")) ?? false;
+			
+			if (isExtensibilityTopic)
+			{
+				return false;
+			}
+			
+			return true;
+		}
     }
 }
