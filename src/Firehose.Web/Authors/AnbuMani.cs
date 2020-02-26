e278b083900a68f0ca0ee58f9a8877afb6bb4a65
James Montemagno <james.montemagno@gmail.com>
2020-02-25 21:30:44 -0800

    Update AnbuMani.cs

--- src/Firehose.Web/Authors/AnbuMani.cs
+++ src/Firehose.Web/Authors/AnbuMani.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 using Firehose.Web.Infrastructure;
-
-public class AnbuMani : IAmACommunityMember
+namespace Firehose.Web.Authors
+{
+	public class AnbuMani : IAmACommunityMember
     {
         public string FirstName => "Anbu";
         public string LastName => "Mani";
@@ -21,4 +22,5 @@ public class AnbuMani : IAmACommunityMember
 
         public string GitHubHandle => "AnbuMani27";
         public string FeedLanguageCode => "en";
+	}
 }
