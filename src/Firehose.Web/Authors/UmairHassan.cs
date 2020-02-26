303b8f52835a3c9dd4ce87ce4acf072ff37f20c3
James Montemagno <james.montemagno@gmail.com>
2020-02-25 21:29:47 -0800

    Update UmairHassan.cs

--- src/Firehose.Web/Authors/UmairHassan.cs
+++ src/Firehose.Web/Authors/UmairHassan.cs
@@ -1,4 +1,4 @@
-using Firehose.Web.Infrastructure;
+/*using Firehose.Web.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,4 +38,4 @@ namespace Firehose.Web.Authors
 
 		public string FeedLanguageCode => "en";
 	}
-}
+}*/
