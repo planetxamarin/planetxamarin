80651e3063bef5a0df86422914baa026079f1441
Tomasz Cielecki <tomasz@ostebaronen.dk>
2020-03-08 17:43:33 +0100

    Fix Preview on Web Site (#649)
    * Fix filtering in preview with new feed
    
    I was filtering the number of items to take before sorting by date.
    
    This was showing the preview with very old posts.
    
    * Fix FeedBurner feeds not showing authors in preview
    
    The URLs are proxied and won't match with author easily
    
    * Fix matching of sites such as MergeConflict
    
    * Trim spaces from name

--- src/Firehose.Web/ViewModels/PreviewViewModel.cs
+++ src/Firehose.Web/ViewModels/PreviewViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Xml;
@@ -14,24 +15,60 @@ namespace Firehose.Web.ViewModels
 
 		public PreviewViewModel(SyndicationFeed feed, IAmACommunityMember[] authors)
 		{
+			bool MatchesAuthorUrls(IAmACommunityMember author, IEnumerable<Uri> urls)
+			{
+				var authorHosts = author.FeedUris.Select(au => au.Host.ToLowerInvariant()).Concat(new[] { author.WebSite.Host.ToLowerInvariant() }).ToArray();
+				var feedBurnerAuthors = author.FeedUris.Where(au => au.Host.Contains("feeds.feedburner")).ToList();
+
+				foreach(var itemUrl in urls)
+				{
+					var host = itemUrl.Host.ToLowerInvariant();
+
+					if (authorHosts.Contains(host))
+						return true;
+
+					if (authorHosts.Contains(host.Replace("www.", "")))
+						return true;
+
+					if (host.Contains("feedproxy.google")) //  feed burner is messed up :(
+					{
+						// url will look like:
+						// feedproxy.google.com/~r/<feedburnerId>/~3/bgJNuDXwkU0/O
+						if (itemUrl.Segments.Count() >= 5)
+						{
+							var feedBurnerId = itemUrl.Segments[2].Trim('/');
+							if (feedBurnerAuthors.Any(fba => fba.AbsoluteUri.Contains(feedBurnerId)))
+								return true;
+						}
+					}
+				}
+
+				return false;
+			}
+
 			var items = new List<PreviewModelItem>();
 			foreach(var item in feed.Items)
 			{
-				var itemUri = item.Links.FirstOrDefault();
-				var author = authors.FirstOrDefault(b => 
-					b.FeedUris.Any(u => itemUri != null && (itemUri.Uri.Host == u.Host || itemUri.Uri.Host.Replace("www.", "") == u.Host.Replace("www.", ""))));
+				var author = authors.FirstOrDefault(a => MatchesAuthorUrls(a, item.Links.Select(l => l.Uri)));
 
 				string authorName;
 
 				if (author != null)
 				{
-					authorName = author.FirstName + " " + author.LastName;
+					authorName = $"{author.FirstName} {author.LastName}".Trim();
 				}
 				// If no author was matched, extract the name from the RSS feed, something is better than nothing right?!
 				else
 				{
 					var creator = item.ElementExtensions.FirstOrDefault(x => x.OuterName == "creator" && x.OuterNamespace == "http://purl.org/dc/elements/1.1/");
-					authorName = creator.GetObject<XmlElement>().Value ?? string.Empty;
+					if (creator != null)
+					{
+						authorName = creator.GetObject<XmlElement>().Value ?? string.Empty;
+					}
+					else
+					{
+						authorName = string.Join(", ", item.Authors.Select(a => $"{a.Name} {a.Email}".Trim()));
+					}
 				}
 
 				var link = item.Links.FirstOrDefault()?.Uri.ToString() ?? string.Empty;
