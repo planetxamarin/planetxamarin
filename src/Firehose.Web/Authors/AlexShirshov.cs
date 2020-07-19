79d21d8fdc03236095884da41c3747f321d41685
Alex Shirshov <shirshov@gmail.com>
2020-07-19 12:33:59 +1000

    Added an author: Alex Shirshov

--- src/Firehose.Web/Authors/AlexShirshov.cs
+++ src/Firehose.Web/Authors/AlexShirshov.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ServiceModel.Syndication;
 using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-	public class AlexShirshov : IAmACommunityMember
+	public class AlexShirshov : IAmACommunityMember, IFilterMyBlogPosts
 	{
 		public string FirstName => "Alex";
 		public string LastName => "Shirshov";
@@ -15,8 +16,10 @@ namespace Firehose.Web.Authors
 		public string TwitterHandle => "omnitalented";
 		public string GitHubHandle => "shirshov";
 		public string GravatarHash => "";
-		public IEnumerable<Uri> FeedUris => new[] {new Uri("https://omnitalented.com/rss-xamarin.xml")};
-		public GeoPosition Position  =>new GeoPosition(-33.882360, 151.206588);
+		public GeoPosition Position  => new GeoPosition(-33.882360, 151.206588);
 		public string FeedLanguageCode => "en";
+		// This is a special feed URL, with Xamarin related posts only
+		public IEnumerable<Uri> FeedUris => new[] {new Uri("https://omnitalented.com/rss-xamarin.xml")};
+		public bool Filter(SyndicationItem item) => true;
 	}
 }
