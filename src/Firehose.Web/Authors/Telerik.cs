1a4c533b29823b7a1eb0bc547cbdd539734b4cae
Lance McCarthy <lance.mccarthy@progress.com>
2020-08-05 12:12:31 -0400

    Update Telerik.cs

--- src/Firehose.Web/Authors/Telerik.cs
+++ src/Firehose.Web/Authors/Telerik.cs
@@ -1,10 +1,12 @@
 ﻿using Firehose.Web.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
 
 namespace Firehose.Web.Authors
 {
-	public class Telerik : IAmACommunityMember
+	public class Telerik : IAmACommunityMember, IFilterMyBlogPosts
 	{
 		public string FirstName => "Telerik";
 
@@ -36,5 +38,28 @@ namespace Firehose.Web.Authors
 		public GeoPosition Position => new GeoPosition(42.512778, -71.251500); 
 
 		public string FeedLanguageCode => "en";
+
+		public bool Filter(SyndicationItem item)
+		{
+			var relevantToXamarin = false;
+
+			if (item.Title.Text.ToLowerInvariant().Contains("xamarin"))
+			{
+				// If the title mentions Xamarin, the topic is primarily and directly relevant.
+				relevantToXamarin = true;
+			}
+			else if (item.Categories?.Any(c => c.Name.ToLowerInvariant().Contains("xamarin")) ?? false)
+			{
+				// If the categories contains Xamarin, the topic directly relevant.
+				relevantToXamarin = true;
+			}
+			else if(item.Content.ToString().ToLowerInvariant().Contains("xamarin"))
+			{
+				// If the body talks about Xamarin, the topic uses Xamarin in some way (i.e. Use Azure function for Xamarin document processing).
+				relevantToXamarin = true;
+			}
+
+			return relevantToXamarin;
+		}
 	}
 }
