c0c4503e20032b3a46b52bb489388d8f08ffb350
Steven Checo <steven.checo.19@gmail.com>
2020-08-31 19:06:42 -0400

    Add Steven Checo Profile

---
+++ src/Firehose.Web/Authors/StevenCheco.cs
@@ -0,0 +1,36 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Firehose.Web.Authors
+{
+	public class StevenCheco : IAmACommunityMember
+	{
+		public string FirstName => "Steven";
+
+		public string LastName => "Checo";
+
+		public string StateOrRegion => "United States";
+
+		public string EmailAddress => "steven.checo.19@gmail.com";
+
+		public string ShortBioOrTagLine => "Software Engineer";
+
+		public Uri WebSite => new Uri("https://checox.com");
+
+		public string TwitterHandle => "steven1909";
+
+		public string GitHubHandle => "sact1909";
+
+		public string GravatarHash => "47ad7178de588bb9a0b5922a5c1364c8";
+
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://checox.com/feed"); } }
+
+		public GeoPosition Position => new GeoPosition(40.8448, 73.8648);
+
+		public string FeedLanguageCode => "Es";
+
+	}
+}
