ab15a02ef28b7f9317f6b8c92cd009983d88e373
Steven Alexander Checo Terrero <steven.checo.19@gmail.com>
2020-09-10 15:57:54 -0400

    Add Steven Checo Profile (#686)
    * Add Steven Checo Profile
    
    * Add StevenCheco.cs to csproj
    
    * Fixed FeedLanguageCode on StevenCheco.cs
    
    * Fixed GeoPosition and ShorBio at StevenCheco.cs
    
    Co-authored-by: James Montemagno <james.montemagno@gmail.com>

---
+++ src/Firehose.Web/Authors/StevenCheco.cs
@@ -0,0 +1,36 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Firehose.Web.Authors
+{
+	public class StevenCheco : IAmACommunityMember
+	{
+		public string FirstName => "Steven";
+
+		public string LastName => "Checo";
+
+		public string StateOrRegion => "United States";
+
+		public string EmailAddress => "steven.checo.19@gmail.com";
+
+		public string ShortBioOrTagLine => "is a C# ASP.Net and Xamarin Developer, I'd preferd back-end but I get along with the Front-End, I love help others with code and build new things with friends, I ♥ C#";
+
+		public Uri WebSite => new Uri("https://checox.com");
+
+		public string TwitterHandle => "steven1909";
+
+		public string GitHubHandle => "sact1909";
+
+		public string GravatarHash => "47ad7178de588bb9a0b5922a5c1364c8";
+
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://checox.com/feed/"); } }
+
+		public GeoPosition Position => new GeoPosition(40.837222, -73.886111);
+
+		public string FeedLanguageCode => "en";
+
+	}
+}
