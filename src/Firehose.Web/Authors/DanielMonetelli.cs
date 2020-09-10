988359d0803d8b9147cf7fd9c14bce83b91cce1a
Daniel Monettelli L <14121125+danielmonettelli@users.noreply.github.com>
2020-09-10 14:58:50 -0500

    Update DanielMonetelli.cs (#678)
    * Update DanielMonetelli.cs
    
    * Update DanielMonettelli.cs
    
    Co-authored-by: Tomasz Cielecki <tomasz@ostebaronen.dk>
    Co-authored-by: James Montemagno <james.montemagno@gmail.com>

--- src/Firehose.Web/Authors/DanielMonetelli.cs
+++ src/Firehose.Web/Authors/DanielMonetelli.cs
@@ -4,33 +4,31 @@ using Firehose.Web.Infrastructure;
 
 namespace Firehose.Web.Authors
 {
-	public class DanielMonetelli : IAmAYoutuber
-	{
-		public string FirstName => "Daniel";
+    public class DanielMonetelli : IAmACommunityMember, IAmAYoutuber
+    {
+        public string FirstName => "Daniel Angel";
+        public string LastName => "Monettelli L.";
+        public string ShortBioOrTagLine => "is a Software Engineer, Videoblogger, Xamarin Mobile Developer & UI/UX Architect.";
+        public string StateOrRegion => "Arequipa, Perú";
+        public string EmailAddress => "danielmonetelli@hotmail.com";
+        public Uri WebSite => new Uri("https://danielmonettelli.github.io");
 
-		public string LastName => "Monetelli";
+        public IEnumerable<Uri> FeedUris
+        {
+            get
+            {
+                return new Uri[]
+                {
+                    new Uri("https://danielmonettelli.github.io/feed.xml"),
+                    new Uri("https://www.youtube.com/feeds/videos.xml?channel_id=UCTAlbAORoFvAHj7jA4DLSFQ")
+                };
+            }
+        }
 
-		public string ShortBioOrTagLine => "is a Xamarin Mobile Developer, UI/UX Architect";
-
-		public string StateOrRegion => "Arequipa, Perú";
-
-		public string EmailAddress => "danielmonetelli@hotmail.com";
-
-		public Uri WebSite => new Uri("https://www.youtube.com/c/danielmonetelli");
-
-		public IEnumerable<Uri> FeedUris
-		{
-			get { yield return new Uri("https://www.youtube.com/feeds/videos.xml?channel_id=UCTAlbAORoFvAHj7jA4DLSFQ"); }
-		}
-
-		public string TwitterHandle => "DanielMonetelli";
-
-
-		public string GravatarHash => "4b3d0e60019ad8fe1e4d7cd5c8850efb";
-		public string GitHubHandle => "monetelli";
-
-		public GeoPosition Position => new GeoPosition(-16.4042643 , -71.5486383);
-
-                public string FeedLanguageCode => "es";
-	 }
+        public string TwitterHandle => "DanielMonetelli";
+        public string GravatarHash => "4b3d0e60019ad8fe1e4d7cd5c8850efb";
+        public string GitHubHandle => "danielmonettelli";
+        public GeoPosition Position => new GeoPosition(-16.4042643, -71.5486383);
+        public string FeedLanguageCode => "en";
+    }
 }
