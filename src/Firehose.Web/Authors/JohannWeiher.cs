cae128b8e40ac59552816cbf850a7ad6245e399b
Johann Weiher <info@suplanus.de>
2020-11-04 21:05:26 +0100

    JohannWeiher added (#697)
    Co-authored-by: Tomasz Cielecki <tomasz@ostebaronen.dk>

---
+++ src/Firehose.Web/Authors/JohannWeiher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+	public class JohannWeiher : IAmACommunityMember
+	{
+		public string FirstName => "Johann";
+		public string LastName => "Weiher";
+		public string ShortBioOrTagLine => "Software Engineer and founder of CodeQuartett & Cuterdio app";
+		public string StateOrRegion => "Germany";
+		public string EmailAddress => "info@suplanus.de";
+		public string TwitterHandle => "JohannWeiher";
+		public string GravatarHash => "482fcc4e05cda49716f00e028c28cfae";
+		public string GitHubHandle => "Suplanus";
+		public GeoPosition Position => new GeoPosition(48.778010, 11.847377);
+		public Uri WebSite => new Uri("https://maui-blog.com/");
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://maui-blog.com/feed/"); } }
+		public string FeedLanguageCode => "en";
+	}
+}
