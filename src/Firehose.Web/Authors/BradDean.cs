42b6f0cfdae7d4c05450e24cf9e0fea4e170e1d7
Brad Dean <brad@truegeek.com>
2021-01-05 16:51:55 -0500

    Adding new author Brad Dean

---
+++ src/Firehose.Web/Authors/BradDean.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+
+namespace Firehose.Web.Authors
+{
+
+	public class BradDean : IAmACommunityMember
+	{
+
+		public string FirstName => "Brad";
+
+		public string LastName => "Dean";
+
+		public string ShortBioOrTagLine => "is a Xamarin mobile developer";
+
+		public string StateOrRegion => "Atlanta";
+
+		public string EmailAddress => "brad@truegeek.com";
+
+		public string TwitterHandle => "BradDeanDev";
+
+		public string GravatarHash => "990a708aaacb12fc6882eb45b4a26ffd";
+
+		public string GitHubHandle => "TrueGeek";
+
+		public GeoPosition Position => new GeoPosition(33.955002, -83.988251);
+
+		public Uri WebSite => new Uri("https://truegeek.com");
+
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://truegeek.com/feed.xml"); } }
+
+        public string FeedLanguageCode => "en";
+
+	}
+
+}
