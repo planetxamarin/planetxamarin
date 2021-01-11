549e68a3dc78444eb499bf890c2105b18779129d
joacim wall <joacim.wall@evry.com>
2021-01-11 08:33:27 +0100

    Fix for All_Authors_Are_In_Authors_Namespace

--- src/Firehose.Web/Authors/JoacimWall.cs
+++ src/Firehose.Web/Authors/JoacimWall.cs
@@ -1,18 +1,21 @@
 using Firehose.Web.Infrastructure;
 using System;
 using System.Collections.Generic;
-public class JoacimWall : IAmACommunityMember
+namespace Firehose.Web.Authors
 {
-    public string FirstName => "Joacim";
-    public string LastName => "Wall";
-    public string ShortBioOrTagLine => "mobility architect";
-    public string StateOrRegion => "Värmland, Sweden";
-    public string EmailAddress => "joacim.wall@gmail.com";
-    public string TwitterHandle => "";
-    public string GravatarHash => "";
-    public string GitHubHandle => "JoacimWall";
-    public GeoPosition Position => new GeoPosition(59.378117005663405, 13.499225556120823);
-    public Uri WebSite => new Uri("https://joacimwall.github.io/");
-    public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://joacimwall.github.io/feed_rss_created.xml"); } }
-    public string FeedLanguageCode => "en";
+	public class JoacimWall : IAmACommunityMember
+	{
+		public string FirstName => "Joacim";
+		public string LastName => "Wall";
+		public string ShortBioOrTagLine => "mobility architect";
+		public string StateOrRegion => "Värmland, Sweden";
+		public string EmailAddress => "joacim.wall@gmail.com";
+		public string TwitterHandle => "";
+		public string GravatarHash => "";
+		public string GitHubHandle => "JoacimWall";
+		public GeoPosition Position => new GeoPosition(59.378117005663405, 13.499225556120823);
+		public Uri WebSite => new Uri("https://joacimwall.github.io/");
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://joacimwall.github.io/feed_rss_created.xml"); } }
+		public string FeedLanguageCode => "en";
+	}
 }
