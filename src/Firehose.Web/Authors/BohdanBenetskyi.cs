2a0afc8cf4e61eeb2d29e34d4666e585050ed048
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-02-02 06:27:37 +0100

    Update BohdanBenetskyi.cs

--- src/Firehose.Web/Authors/BohdanBenetskyi.cs
+++ src/Firehose.Web/Authors/BohdanBenetskyi.cs
@@ -1,33 +1,33 @@
-﻿//using Firehose.Web.Infrastructure;
-//using System;
-//using System.Collections.Generic;
+using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
 
-//namespace Firehose.Web.Authors
-//{
-//	public class BohdanBenetskyi : IAmACommunityMember
-//	{
-//		public string FirstName => "Bohdan";
+namespace Firehose.Web.Authors
+{
+	public class BohdanBenetskyi : IAmACommunityMember
+	{
+		public string FirstName => "Bohdan";
 
-//		public string LastName => "Benetskyi";
+		public string LastName => "Benetskyi";
 
-//		public string StateOrRegion => "Rzeszow, Poland";
+		public string StateOrRegion => "Rzeszow, Poland";
 
-//		public string EmailAddress => "benetskyybogdan@gmail.com";
+		public string EmailAddress => "benetskyybogdan@gmail.com";
 
-//		public string ShortBioOrTagLine => "Xamarin Software Developer, co-organizer of Xamarin Local Events in Rzeszow, Local CSS at Xamarin Advocate";
+		public string ShortBioOrTagLine => "Xamarin Software Developer, co-organizer of Xamarin Local Events in Rzeszow, Local CSS at Xamarin Advocate";
 
-//		public Uri WebSite => new Uri("https://medium.com/@benetskyybogdan/");
+		public Uri WebSite => new Uri("https://benetskyybogdan.medium.com/");
 
-//		public string TwitterHandle => "bbenetskyy";
+		public string TwitterHandle => "bbenetskyy";
 
-//		public string GitHubHandle => "bbenetskyy";
+		public string GitHubHandle => "bbenetskyy";
 
-//		public string GravatarHash => "8bfa7db9239c2969b79468a58c8dd066";
+		public string GravatarHash => "8bfa7db9239c2969b79468a58c8dd066";
 
-//		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://medium.com/feed/@benetskyybogdan/"); } }
+		public IEnumerable<Uri> FeedUris { get { yield return new Uri("https://medium.com/feed/@benetskyybogdan/"); } }
 
-//		public GeoPosition Position => new GeoPosition(50.041187, 21.999121);
+		public GeoPosition Position => new GeoPosition(50.041187, 21.999121);
 
-//		public string FeedLanguageCode => "en";
-//	}
-//}
+		public string FeedLanguageCode => "en";
+	}
+}
