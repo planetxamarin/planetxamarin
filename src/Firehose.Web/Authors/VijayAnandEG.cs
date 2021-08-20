0cdcc98cd0e51d4fbe481bd72352b7fde1ee7d12
Tomasz Cielecki <tomasz@ostebaronen.dk>
2021-08-20 09:14:37 +0200

    Remove filter. We filter for Xamarin per default

--- src/Firehose.Web/Authors/VijayAnandEG.cs
+++ src/Firehose.Web/Authors/VijayAnandEG.cs
@@ -6,7 +6,7 @@ using System.ServiceModel.Syndication;
 
 namespace Firehose.Web.Authors
 {
-	public class VijayAnandEG : IAmACommunityMember, IFilterMyBlogPosts
+	public class VijayAnandEG : IAmACommunityMember
 	{
 		public string FirstName => "Vijay Anand";
 
@@ -31,19 +31,5 @@ namespace Firehose.Web.Authors
 		public GeoPosition Position => new GeoPosition(12.986768, 80.2117061);
 
 		public string FeedLanguageCode => "en";
-
-		// As am planning to publish articles on non-Xamarin .NET stuff like Blazor
-		// Hence introducing the filter
-		public bool Filter(SyndicationItem item)
-		{
-			if (item.Title?.Text?.ToLowerInvariant().Contains("xamarin") ?? false)
-			{
-				return true;
-			}
-
-			// This filters out only the posts that have the "xamarin" category
-			// Not all blog posts have categories, please guard against this
-			return item.Categories?.Any(c => c.Name.ToLowerInvariant().Contains("xamarin")) ?? false;
-		}
 	}
 }
