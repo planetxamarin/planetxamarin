44d4c94f7c2f33cee98b15c45fd52b7a005dbbde
Glenn Versweyveld <Depechie@users.noreply.github.com>
2022-03-30 04:22:22 +0200

    Added author Glenn Versweyveld (#726)

---
+++ src/Firehose.Web/Authors/GlennVersweyveld.cs
@@ -0,0 +1,27 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Firehose.Web.Authors
+{
+    public class GlennVersweyveld : IAmACommunityMember
+    {
+        public string FirstName => "Glenn";
+        public string LastName => "Versweyveld";
+        public string ShortBioOrTagLine => "'SH10151' this is the serial number of our orbital gun";
+        public string StateOrRegion => "Antwerp, Belgium";
+        public string EmailAddress => "Glenn.Versweyveld@gmail.com";
+        public string TwitterHandle => "depechie";
+        public GeoPosition Position => new GeoPosition(51.091310, 4.548315);
+        public Uri WebSite => new Uri("https://blog.depechie.com");
+
+        public IEnumerable<Uri> FeedUris
+        {
+            get { yield return new Uri("https://blog.depechie.com/index.xml"); }
+        }
+
+        public string GravatarHash => "d3ac2144120f923f23c49ee4ce4437ab";
+        public string GitHubHandle => "depechie";
+		public string FeedLanguageCode => "en";
+    }
+}
