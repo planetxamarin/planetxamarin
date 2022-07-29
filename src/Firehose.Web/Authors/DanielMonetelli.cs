689b0dfb353ab4a88b229fa67e346a38c59cbc0c
Daniel Monettelli <danielmonetelli@hotmail.com>
2022-07-29 02:48:39 -0500

    Updated DanielMonetelli.cs

--- src/Firehose.Web/Authors/DanielMonetelli.cs
+++ src/Firehose.Web/Authors/DanielMonetelli.cs
@@ -6,12 +6,12 @@ namespace Firehose.Web.Authors
 {
 	public class DanielMonetelli : IAmACommunityMember
 	{
-		public string FirstName => "Daniel Angel";
-		public string LastName => "Monettelli L.";
-		public string ShortBioOrTagLine => "is a Software Engineer, Videoblogger, Xamarin Mobile Developer & UI/UX Architect.";
+		public string FirstName => "Daniel";
+		public string LastName => "Monettelli";
+		public string ShortBioOrTagLine => "is a Software Engineer and UX Engineer";
 		public string StateOrRegion => "Arequipa, Perú";
 		public string EmailAddress => "danielmonetelli@hotmail.com";
-		public Uri WebSite => new Uri("https://danielmonettelli.github.io");
+		public Uri WebSite => new Uri("https://danimonettelli.github.io");
 
 		public IEnumerable<Uri> FeedUris
 		{
@@ -19,15 +19,15 @@ namespace Firehose.Web.Authors
 			{
 				return new Uri[]
 				{
-					new Uri("https://danielmonettelli.github.io/feed.xml"),
+					new Uri("https://danimonettelli.github.io/feed.xml"),
 					new Uri("https://www.youtube.com/feeds/videos.xml?channel_id=UCTAlbAORoFvAHj7jA4DLSFQ")
 				};
 			}
 		}
 
-		public string TwitterHandle => "DanielMonetelli";
+		public string TwitterHandle => "DaniMonettelli";
 		public string GravatarHash => "4b3d0e60019ad8fe1e4d7cd5c8850efb";
-		public string GitHubHandle => "danielmonettelli";
+		public string GitHubHandle => "danimonettelli";
 		public GeoPosition Position => new GeoPosition(-16.4042643, -71.5486383);
 		public string FeedLanguageCode => "en";
 	}
