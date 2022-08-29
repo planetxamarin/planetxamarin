a6fb87add5d39b6cbf6bf243ba160307fabf7a47
Jon Galloway <jongalloway@gmail.com>
2022-08-29 14:26:13 -0700

    Added services (replacing autofac)

--- src/Firehose.WebCore/Program.cs
+++ src/Firehose.WebCore/Program.cs
@@ -1,8 +1,20 @@
+using Firehose.Web.Infrastructure;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.SystemWebAdapters;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSystemWebAdapters();
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddSingleton<NewCombinedFeedSource>();
+
+var members = Assembly.GetCallingAssembly().GetTypes()
+	.Where(type => typeof(IAmACommunityMember).IsAssignableFrom(type) && !type.IsInterface);
+
+foreach (var member in members)
+	builder.Services.AddTransient(typeof(IAmACommunityMember), member);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
