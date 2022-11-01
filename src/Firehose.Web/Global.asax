22466efa7a48fdcf5e24ea11f11c9e0b9b3d4893
James Montemagno <jamont@microsoft.com>
2022-11-01 10:44:50 -0700

    Remove old and update prs

--- src/Firehose.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Firehose.Web.MvcApplication" Language="C#" %>
--- src/Firehose.Web/Infrastructure/IFilterMyBlogPosts.cs
+++
@@ -1,9 +0,0 @@
-using System.ServiceModel.Syndication;
-
-namespace Firehose.Web.Infrastructure
-{
-    public interface IFilterMyBlogPosts
-    {
-        bool Filter(SyndicationItem item);
-    }
-}
