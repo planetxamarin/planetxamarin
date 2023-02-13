f75c221b541fa4daf63ce709af1c31ff3395b0d8
Jean-Emmanuel BAILLAT <jeanemmanuel.baillat@gmail.com>
2023-02-13 20:35:37 +0100

    add blog mauicestclair.fr (#747)
    * add blog mauicestclair.fr
    
    * Fix wrong namespace
    
    * Fix unexpected removal due to VS for Mac

---
+++ src/Firehose.WebCore/Authors/JeanEmmanuelBaillat.cs
@@ -0,0 +1,25 @@
+﻿using Firehose.Web.Infrastructure;
+using System;
+
+namespace Firehose.Web.Authors
+{
+	public class JeanEmmanuelBaillat : IAmACommunityMember
+	{
+		public string FirstName => "Jean-Emmanuel";
+		public string LastName => "Baillat";
+		public string StateOrRegion => "Nice, France";
+		public string EmailAddress => "jeanemmanuel.baillat@gmail.com";
+		public string ShortBioOrTagLine => "(or J-E), is a Software Engineer and a French blogger about .NET MAUI. Any beginner developer should quickly have fun and produce cool apps!";
+		public Uri WebSite => new Uri("https://www.mauicestclair.fr/");
+		public string TwitterHandle => "kapuschinfo";
+		public string GitHubHandle => "kapusch";
+		public string GravatarHash => "52e9c14f8ea010d1e88eb4a4559cfa3b";
+		public IEnumerable<Uri> FeedUris
+		{
+			get { yield return new Uri("https://www.mauicestclair.fr/index.xml"); }
+		}
+		public GeoPosition Position => new GeoPosition(43.700000, 7.250000);
+		public string FeedLanguageCode => "fr";
+	}
+}
+
